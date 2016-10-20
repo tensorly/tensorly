047a499ea5423dc2cd3b3d694c1496f9d07ec35d
Jean KOSSAIFI <jean.kossaifi@gmail.com>
2016-10-20 17:51:10 +0100

    Sensitive imports

---
+++ tensorly/__init__.py
@@ -0,0 +1,8 @@
+from .base import unfold, fold
+from .base import tensor_to_vec, vec_to_tensor
+from .base import partial_unfold, partial_fold
+from .base import partial_tensor_to_vec, partial_vec_to_tensor
+from .base import tensor_from_frontal_slices
+
+from .kruskal import kruskal_to_tensor, kruskal_to_unfolded, kruskal_to_vec
+from .tucker import tucker_to_tensor, tucker_to_unfolded, tucker_to_vec
