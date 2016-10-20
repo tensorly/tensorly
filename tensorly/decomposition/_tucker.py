60bad71b9e684035e65575cef12c12143f6cc888
Jean KOSSAIFI <jean.kossaifi@gmail.com>
2016-10-20 19:01:00 +0100

    Tucker decomposition

---
+++ tensorly/decomposition/_tucker.py
@@ -0,0 +1,166 @@
+import numpy as np
+from ..base import unfold
+from ..tenalg import multi_mode_dot, mode_dot, norm
+from ..tenalg import partial_svd
+from ..tucker import tucker_to_tensor
+from ..utils import check_random_state
+
+
+def tucker(tensor, ranks=None, n_iter_max=100, init='svd', tol=10e-5,
+           random_state=None, verbose=False):
+    """Tucker decomposition via Higher Order Orthogonal Iteration (HOI)
+
+        Decomposes `tensor` into a Tucker decomposition:
+        ``tensor = [| core; factors[0], ...factors[-1] |]``
+
+    Parameters
+    ----------
+    tensor : ndarray
+    ranks : None or int list
+            size of the core tensor, ``(len(ranks) == tensor.ndim)``
+    n_iter_max : int
+                 maximum number of iteration
+    init : {'svd', 'random'}, optional
+    tol : float, optional
+          tolerance: the algorithm stops when the variation in
+          the reconstruction error is less than the tolerance
+    random_state : {None, int, np.random.RandomState}
+    verbose : int, optional
+        level of verbosity
+
+    Returns
+    -------
+    core : ndarray of size `ranks`
+            core tensor of the Tucker decomposition
+    factors : ndarray list
+            list of factors of the Tucker decomposition.
+            Its ``i``-th element is of shape ``(tensor.shape[i], ranks[i])``
+    """
+    if ranks is None:
+        ranks = [s for s in tensor.shape]
+
+    # SVD init
+    if init == 'svd':
+        factors = []
+        for mode in range(tensor.ndim):
+            eigenvecs, _, _ = partial_svd(unfold(tensor, mode), n_eigenvecs=ranks[mode])
+            factors.append(eigenvecs)
+    else:
+        rng = check_random_state(random_state)
+        core = rng.random_sample(ranks)
+        factors = [rng.random_sample(s) for s in zip(tensor.shape, ranks)]
+
+    if verbose or tol:
+        rec_errors = []
+        norm_tensor = norm(tensor, 2)
+
+    for iteration in range(n_iter_max):
+        for mode in range(tensor.ndim):
+            core_approximation = tucker_to_tensor(tensor, factors, skip_factor=mode, transpose_factors=True)
+            eigenvecs, _, _ = partial_svd(unfold(core_approximation, mode), n_eigenvecs=ranks[mode])
+            factors[mode] = eigenvecs
+
+        core = tucker_to_tensor(tensor, factors, transpose_factors=True)
+
+        if verbose or tol:
+            rec_error = norm(tensor - tucker_to_tensor(core, factors), 2) / norm_tensor
+            rec_errors.append(rec_error)
+
+            if iteration > 1:
+                if verbose:
+                    print('reconsturction error={}, variation={}.'.format(
+                        rec_errors[-1], rec_errors[-2] - rec_errors[-1]))
+
+                if tol and abs(rec_errors[-2] - rec_errors[-1]) < tol:
+                    if verbose:
+                        print('converged in {} iterations.'.format(iteration))
+                    break
+
+    return core, factors
+
+
+def non_negative_tucker(tensor, ranks, n_iter_max=10, init='svd', tol=10e-5,
+                        random_state=None, verbose=False):
+    """Non-negative Tucker decomposition
+
+        Iterative multiplicative update, see [1]_, [2]_
+
+    Parameters
+    ----------
+    tensor : ``ndarray``
+    rank   : int
+            number of components
+    n_iter_max : int
+                 maximum number of iteration
+    init : {'svd', 'random'}
+    random_state : {None, int, np.random.RandomState}
+
+    Returns
+    -------
+    core : ndarray
+            positive core of the Tucker decomposition
+            has shape `ranks`
+    factors : ndarray list
+            list of factors of the CP decomposition
+            element `i` is of shape (tensor.shape[i], rank)
+
+    References
+    ----------
+    .. [1] G. Zhou, A. Cichocki, Q. Zhao and S. Xie,
+       "Nonnegative Matrix and Tensor Factorizations : An algorithmic perspective,"
+       in IEEE Signal Processing Magazine, vol. 31, no. 3, pp. 54-65, May 2014.
+    .. [2] DD Lee and HS Seung,
+       "Algorithms for non-negative matrix factorization",
+       in Advances in neural information processing systems (NIPS), 2001
+    """
+    epsilon = 10e-12
+
+    # Initialisation
+    if init == 'svd':
+        core, factors = tucker(tensor, ranks)
+        nn_factors = [np.abs(f) for f in factors]
+        nn_core = np.abs(core)
+    else:
+        rng = check_random_state(random_state)
+        core = rng.random_sample(ranks) + 0.01  # Check this
+        factors = [rng.random_sample(s) for s in zip(tensor.shape, ranks)]
+        nn_factors = [np.abs(f) for f in factors]
+        nn_core = np.abs(core)
+
+    n_factors = len(nn_factors)
+    norm_tensor = norm(tensor, 2)
+    rec_errors = []
+
+    for iteration in range(n_iter_max):
+        for mode in range(tensor.ndim):
+            B = tucker_to_tensor(nn_core, nn_factors, skip_factor=mode)
+            B = unfold(B, mode).T
+
+            numerator = np.dot(unfold(tensor, mode), B)
+            numerator = numerator.clip(min=epsilon)
+            denominator = np.dot(nn_factors[mode], B.T.dot(B))
+            denominator = denominator.clip(min=epsilon)
+            nn_factors[mode] *= numerator / denominator
+
+        numerator = tucker_to_tensor(tensor, nn_factors, transpose_factors=True)
+        numerator = numerator.clip(min=epsilon)
+        for i, f in enumerate(nn_factors):
+            if i:
+                denominator = mode_dot(denominator, f.T.dot(f), i)
+            else:
+                denominator = mode_dot(nn_core, f.T.dot(f), i)
+        denominator = denominator.clip(min=epsilon)
+        nn_core *= numerator / denominator
+
+        rec_error = norm(tensor - tucker_to_tensor(nn_core, nn_factors), 2) / norm_tensor
+        rec_errors.append(rec_error)
+        if iteration > 1 and verbose:
+            print('reconsturction error={}, variation={}.'.format(
+                rec_errors[-1], rec_errors[-2] - rec_errors[-1]))
+
+        if iteration > 1 and abs(rec_errors[-2] - rec_errors[-1]) < tol:
+            if verbose:
+                print('converged in {} iterations.'.format(iteration))
+            break
+
+    return nn_core, nn_factors
---
+++ tensorly/decomposition/tests/test_tucker.py
@@ -0,0 +1,87 @@
+from numpy.testing import assert_, assert_equal, assert_array_almost_equal
+import numpy as np
+from .._tucker import tucker, non_negative_tucker
+from ...tucker import tucker_to_tensor
+from ...tenalg import norm
+from ...utils import check_random_state
+
+
+def test_tucker():
+    """Test for the Tucker decomposition"""
+    rng = check_random_state(1234)
+
+    tol_norm_2 = 10e-3
+    tol_max_abs = 10e-1
+    tensor = rng.random_sample((3, 4, 3))
+    core, factors = tucker(tensor, ranks=None, n_iter_max=200, verbose=True)
+    reconstructed_tensor = tucker_to_tensor(core, factors)
+    norm_rec = norm(reconstructed_tensor, 2)
+    norm_tensor = norm(tensor, 2)
+    assert_((norm_rec - norm_tensor)/norm_rec < tol_norm_2)
+
+    # Test the max abs difference between the reconstruction and the tensor
+    assert_(np.max(np.abs(norm_rec - norm_tensor)) < tol_max_abs)
+
+    # Test the shape of the core and factors
+    ranks = [2, 3, 1]
+    core, factors = tucker(tensor, ranks=ranks, n_iter_max=100, verbose=1)
+    for i, rank in enumerate(ranks):
+        assert_equal(factors[i].shape, (tensor.shape[i], ranks[i]),
+                     err_msg="factors[{}].shape={}, expected {}".format(
+                         i, factors[i].shape, (tensor.shape[i], ranks[i])))
+        assert_equal(core.shape[i], rank, err_msg="Core.shape[{}]={}, "
+                     "expected {}".format(i, core.shape[i], rank))
+
+    # Random and SVD init should converge to a similar solution
+    tol_norm_2 = 10e-1
+    tol_max_abs = 10e-1
+
+    core_svd, factors_svd = tucker(tensor, ranks=[3, 4, 3], n_iter_max=200, init='svd', verbose=1)
+    core_random, factors_random = tucker(tensor, ranks=[3, 4, 3], n_iter_max=200, init='random')
+    rec_svd = tucker_to_tensor(core_svd, factors_svd)
+    rec_random = tucker_to_tensor(core_random, factors_random)
+    error = norm(rec_svd - rec_random, 2)
+    error /= norm(rec_svd, 2)
+    assert_(error < tol_norm_2,
+            'norm 2 of difference between svd and random init too high')
+    assert_(np.max(np.abs(rec_svd - rec_random)) < tol_max_abs,
+            'abs norm of difference between svd and random init too high')
+
+
+
+def test_non_negative_tucker():
+    """Test for non-negative Tucker"""
+    rng = check_random_state(1234)
+
+    tol_norm_2 = 10e-1
+    tol_max_abs = 10e-1
+    tensor = rng.random_sample((3, 4, 3)) + 1
+    core, factors = tucker(tensor, ranks=[3, 4, 3], n_iter_max=200, verbose=1)
+    nn_core, nn_factors = non_negative_tucker(tensor, ranks=[3, 4, 3], n_iter_max=100)
+
+    # Make sure all components are positive
+    for factor in nn_factors:
+        assert_(np.all(factor >= 0))
+    assert_(np.all(nn_core >= 0))
+
+    reconstructed_tensor = tucker_to_tensor(core, factors)
+    nn_reconstructed_tensor = tucker_to_tensor(nn_core, nn_factors)
+    error = norm(reconstructed_tensor - nn_reconstructed_tensor, 2)
+    error /= norm(reconstructed_tensor, 2)
+    assert_(error < tol_norm_2,
+            'norm 2 of reconstruction error higher than tol')
+
+    # Test the max abs difference between the reconstruction and the tensor
+    assert_(np.max(np.abs(reconstructed_tensor - nn_reconstructed_tensor)) < tol_max_abs,
+            'abs norm of reconstruction error higher than tol')
+
+    core_svd, factors_svd = non_negative_tucker(tensor, ranks=[3, 4, 3], n_iter_max=500, init='svd', verbose=1)
+    core_random, factors_random = non_negative_tucker(tensor, ranks=[3, 4, 3], n_iter_max=200, init='random')
+    rec_svd = tucker_to_tensor(core_svd, factors_svd)
+    rec_random = tucker_to_tensor(core_random, factors_random)
+    error = norm(rec_svd - rec_random, 2)
+    error /= norm(rec_svd, 2)
+    assert_(error < tol_norm_2,
+            'norm 2 of difference between svd and random init too high')
+    assert_(np.max(np.abs(rec_svd - rec_random)) < tol_max_abs,
+            'abs norm of difference between svd and random init too high')
