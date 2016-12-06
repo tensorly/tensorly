471c64347758987804981f00843e486a1d5f933d
Jean Kossaifi <jean.kossaifi@gmail.com>
2016-12-06 18:15:05 +0100

    Creating low rank random matrices.

--- tensorly/datasets/tests/test_synthetic.py
+++ tensorly/datasets/tests/test_synthetic.py
@@ -1,6 +1,7 @@
-from ..synthetic import gen_image
-from numpy.testing import assert_array_equal
-import numpy as np
+from ..synthetic import gen_image, low_rank_matrix 
+from numpy.testing import assert_array_equal, assert_equal 
+from numpy.linalg import matrix_rank 
+import numpy as np 
 
 
 def test_gen_image():
@@ -32,3 +33,13 @@ def test_gen_image():
                          [0., 0., 0., 0., 0., 0., 0., 0., 0., 0.]])
     res = gen_image(region='rectangle', image_height=10, image_width=10, weight_value=1)
     assert_array_equal(true_res, res)
+
+
+
+ 
+def test_low_rank_matrix(): 
+    """Test for low_rank_matrix""" 
+    rank = 5 
+    mat = low_rank_matrix((100, 1000), rank) 
+    assert_equal(matrix_rank(mat), rank) 
+
