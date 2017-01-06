e06106f4aaa08918640c06228ab786478389055f
Jean Kossaifi <jean.kossaifi@gmail.com>
2017-01-06 16:50:07 +0000

    Doc: remove breadcrumbs

--- doc/conf.py
+++ doc/conf.py
@@ -200,7 +200,7 @@ html_use_index = False
 html_show_sourcelink = False
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-#html_show_sphinx = True
+html_show_sphinx = False
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
 #html_show_copyright = True
