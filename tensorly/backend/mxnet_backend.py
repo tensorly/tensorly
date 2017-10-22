c06cea808b5f2a1d63827d6a37c5c4f2336417f1
Jean Kossaifi <jean.kossaifi@gmail.com>
2017-10-22 23:15:14 +0200

    Updated MXNet backend
    Native kronecker and khatri-rao products + refactoring.

--- tensorly/backend/mxnet_backend.py
+++ tensorly/backend/mxnet_backend.py
@@ -7,7 +7,7 @@ import scipy.linalg
 import scipy.sparse.linalg
 from numpy import testing
 import mxnet as mx
-from mxnet import nd as np
+from mxnet import nd as nd
 from . import numpy_backend
 
 from mxnet.ndarray import arange, zeros, zeros_like, ones
@@ -15,7 +15,7 @@ from mxnet.ndarray import reshape, moveaxis, dot, transpose
 from mxnet.ndarray import sqrt, abs, where, maximum, sign
 
 
-#import numpy as np
+#import numpy as nd
 # Author: Jean Kossaifi
 
 # License: BSD 3 clause
@@ -25,7 +25,7 @@ def tensor(data, ctx=mx.cpu(), dtype="float64"):
     """
     if dtype is None and isinstance(data, numpy.ndarray):
         dtype = data.dtype
-    return np.array(data, ctx=ctx, dtype=dtype)
+    return nd.array(data, ctx=ctx, dtype=dtype)
 
 def to_numpy(tensor):
     """Convert a tensor to numpy format
@@ -38,12 +38,12 @@ def to_numpy(tensor):
     -------
     ndarray
     """
-    if isinstance(tensor, np.NDArray):
+    if isinstance(tensor, nd.NDArray):
         return tensor.asnumpy()
     elif isinstance(tensor, numpy.ndarray):
         return tensor
     else:
-        raise ValueError('Only mx.nd.array or np.ndarray) are accepted,'
+        raise ValueError('Only mx.nd.array or nd.ndarray) are accepted,'
                          'given {}'.format(type(tensor)))
 
 
@@ -64,16 +64,21 @@ def ndim(tensor):
     return tensor.ndim
 
 def kron(matrix1, matrix2):
-    return tensor(numpy.kron(to_numpy(matrix1), to_numpy(matrix2)))
+    """Kronecker product"""
+    s1, s2 = matrix1.shape
+    s3, s4 = matrix2.shape
+    return nd.reshape(
+                matrix1.reshape((s1, 1, s2, 1))*matrix2.reshape((1, s3, 1, s4)),
+                (s1*s3, s2*s4))
 
 def solve(matrix1, matrix2):
     return tensor(numpy.linalg.solve(to_numpy(matrix1), to_numpy(matrix2)))
 
 def min(tensor, *args, **kwargs):
-    return np.min(tensor, *args, **kwargs).asscalar()
+    return nd.min(tensor, *args, **kwargs).asscalar()
 
 def max(tensor, *args, **kwargs):
-    return np.min(tensor, *args, **kwargs).asscalar()
+    return nd.min(tensor, *args, **kwargs).asscalar()
 
 def norm(tensor, order):
     """Computes the l-`order` norm of tensor
@@ -87,13 +92,13 @@ def norm(tensor, order):
         l-`order` norm of tensor
     """
     if order == 'inf':
-        return np.max(np.abs(tensor)).asscalar()
+        return nd.max(nd.abs(tensor)).asscalar()
     if order == 1:
-        res =  np.sum(np.abs(tensor))
+        res =  nd.sum(nd.abs(tensor))
     elif order == 2:
-        res = np.sqrt(np.sum(tensor**2))
+        res = nd.sqrt(nd.sum(tensor**2))
     else:
-        res = np.sum(np.abs(tensor)**order)**(1/order)
+        res = nd.sum(nd.abs(tensor)**order)**(1/order)
     return res.asscalar()
 
 def kr(matrices):
@@ -123,8 +128,20 @@ def kr(matrices):
          \\text{If every matrix } U_k \\text{ is of size } (I_k \\times R),\\\\
          \\text{Then } \\left(U_1 \\bigodot \\cdots \\bigodot U_n \\right) \\text{ is of size } (\\prod_{k=1}^n I_k \\times R)
     """
-    return tensor(numpy_backend.kr([to_numpy(matrix) for matrix in matrices]))
+    if len(matrices) < 2:
+        raise ValueError('kr requires a list of at least 2 matrices, but {} given.'.format(len(matrices)))
 
+    n_col = shape(matrices[0])[1]
+    for i, e in enumerate(matrices[1:]):
+        if not i:
+            res = matrices[0]
+        s1, s2 = shape(res)
+        s3, s4 = shape(e)
+        if not s2 == s4 == n_col:
+            raise ValueError('All matrices should have the same number of columns.')
+        res = reshape(reshape(res, (s1, 1, s2))*reshape(e, (1, s3, s4)),
+                      (-1, n_col))
+    return res
 
 def partial_svd(matrix, n_eigenvecs=None):
     """Computes a fast partial SVD on `matrix`
@@ -147,7 +164,6 @@ def partial_svd(matrix, n_eigenvecs=None):
         of shape (n_eigenvecs, )
         contains the singular values of `matrix`
     V : 2D-array
-        of shape (n_eigenvecs, matrix.shape[1])
         contains the left singular vectors
     """
     # Check that matrix is... a matrix!
@@ -185,36 +201,36 @@ def partial_svd(matrix, n_eigenvecs=None):
         U, S, V = U[:, ::-1], S[::-1], V[:, ::-1]
         return tensor(U), tensor(S), tensor(V.T)
 
-def clip(tensor, a_min=None, a_max=None, inplace=False):
+def clip(tensor, a_min=None, a_max=None, indlace=False):
     if a_min is not None and a_max is not None:
-        if inplace:
-            np.max(np.min(tensor, a_max, out=tensor), a_min, out=tensor)
+        if indlace:
+            nd.max(nd.min(tensor, a_max, out=tensor), a_min, out=tensor)
         else:
-            tensor = np.maximum(np.minimum(tensor, a_max), a_min)
+            tensor = nd.maximum(nd.minimum(tensor, a_max), a_min)
     elif min is not None:
-        if inplace:
-            np.max(tensor, a_min, out=tensor)
+        if indlace:
+            nd.max(tensor, a_min, out=tensor)
         else:
-            tensor = np.maximum(tensor, a_min)
+            tensor = nd.maximum(tensor, a_min)
     elif max is not None:
-        if inplace:
-            np.min(tensor, a_max, out=tensor)
+        if indlace:
+            nd.min(tensor, a_max, out=tensor)
         else:
-            tensor = np.minimum(tensor, a_max)
+            tensor = nd.minimum(tensor, a_max)
     return tensor
 
 def all(tensor):
-    return np.sum(tensor != 0).asscalar()
+    return nd.sum(tensor != 0).asscalar()
 
 def mean(tensor, *args, **kwargs):
-    res = np.mean(tensor, *args, **kwargs)
+    res = nd.mean(tensor, *args, **kwargs)
     if res.shape == (1,):
         return res.asscalar()
     else:
         return res
 
 def sum(tensor, *args, **kwargs):
-    res = np.sum(tensor, *args, **kwargs)
+    res = nd.sum(tensor, *args, **kwargs)
     if res.shape == (1,):
         return res.asscalar()
     else:
