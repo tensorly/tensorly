fa4951a9076d774b155886b85db09d07aec27ecd
Jean Kossaifi <jean.kossaifi@gmail.com>
2018-02-19 17:55:46 +0000

    Add more metrics

--- tensorly/metrics/regression.py
+++ tensorly/metrics/regression.py
@@ -1,10 +1,9 @@
 from .. import backend as T
-from math import sqrt
 
 # Author: Jean Kossaifi <jean.kossaifi+tensors@gmail.com>
 
 
-def MSE(y_true, y_pred):
+def MSE(y_true, y_pred, axis=None):
     """Returns the mean squared error between the two predictions
 
     Parameters
@@ -18,10 +17,10 @@ def MSE(y_true, y_pred):
     -------
     float
     """
-    return T.mean((y_true - y_pred) ** 2)
+    return T.mean((y_true - y_pred) ** 2, axis=axis)
 
 
-def RMSE(y_true, y_pred):
+def RMSE(y_true, y_pred, axis=None):
     """Returns the regularised mean squared error between the two predictions
     (the square-root is applied to the mean_squared_error)
 
@@ -36,5 +35,51 @@ def RMSE(y_true, y_pred):
     -------
     float
     """
-    return sqrt(MSE(y_true, y_pred))
+    return T.sqrt(MSE(y_true, y_pred, axis=axis))
 
+
+def reflective_correlation_coefficient(y_true, y_pred, axis=None):
+    """Reflective variant of Pearson's product moment correlation coefficient
+    where the predictions are not centered around their mean values.
+
+    Parameters
+    ----------
+    y_true : array of shape (n_samples, )
+        Ground truth (correct) target values.
+    y_pred : array of shape (n_samples, )
+        Estimated target values.
+
+    Returns
+    -------
+    float: reflective correlation coefficient
+    """
+    return T.sum(y_true*y_pred, axis=axis)/T.sqrt(T.sum(y_true**2, axis=axis)*T.sum(y_pred**2, axis=axis))
+
+
+def covariance(y_true, y_pred, axis=None):
+    centered_true = T.mean(y_true, axis=axis)
+    centered_pred = T.mean(y_pred, axis=axis)
+
+    if axis is not None:
+        # TODO: write a function to do this..
+        shape = list(T.shape(y_true))
+        shape[axis] = 1
+        centered_true = T.reshape(centered_true, shape)
+        shape = list(T.shape(y_pred))
+        shape[axis] = 1
+        centered_pred = T.reshape(centered_pred, shape)
+
+    return T.mean((y_true - centered_true)*(y_pred - centered_pred), axis=axis)
+
+
+def variance(y, axis=None):
+    return covariance(y, y, axis=axis)
+
+
+def standard_deviation(y, axis=None):
+    return T.sqrt(variance(y, axis=axis))
+
+
+def correlation(y_true, y_pred, axis=None):
+    """Pearson's product moment correlation coefficient"""
+    return covariance(y_true, y_pred, axis=axis)/T.sqrt(variance(y_true, axis)*variance(y_pred, axis))
--- tensorly/metrics/tests/test_regression.py
+++ tensorly/metrics/tests/test_regression.py
@@ -1,17 +1,35 @@
+import numpy as np
 from ... import backend as T
-from ..regression import MSE, RMSE
+from ..regression import MSE, RMSE, correlation
 
 def test_MSE():
     """Test for MSE"""
-    y_true = T.tensor([1, 0, 1.5, 0.5])
-    y_pred = T.tensor([1, 1, 1, 1])
-    true_mse = 1.5
-    T.assert_(MSE(y_true, y_pred), true_mse)
-
+    y_true = T.tensor([1, 0, 2, -2])
+    y_pred = T.tensor([1, -1, 1, -1])
+    true_mse = 0.75
+    T.assert_array_almost_equal(MSE(y_true, y_pred), true_mse)
 
 def test_RMSE():
     """Test for RMSE"""
-    y_true = T.tensor([1, 2, 0.5, 1.5, 0.5, -1, -1])
-    y_pred = T.tensor([1, 2, 1, 1, 1, -0.5, 0])
-    true_mse = 2
-    T.assert_(MSE(y_true, y_pred), true_mse)
+    y_true = T.tensor([1, 0, 2, -2])
+    y_pred = T.tensor([0, -1, 1, -1])
+    true_mse = 1
+    T.assert_array_almost_equal(MSE(y_true, y_pred), true_mse)
+
+def test_correlation():
+    """Test for correlation"""
+    a = T.tensor(np.random.random(10))
+    b = T.tensor(np.random.random(10))
+    T.assert_array_almost_equal(correlation(a, a*2+1), 1)
+    T.assert_array_almost_equal(correlation(a, -a*2+1), -1)
+    
+    a = T.tensor([1, 2, 3, 2, 1])
+    b = T.tensor([1, 2, 3, 4, 5])
+    T.assert_array_almost_equal(correlation(a, b), 0)
+
+    a = T.tensor([[1, 2, 3, 2, 1]]*3)
+    b = T.tensor([[1, 2, 3, 4, 5]]*3)
+    res = T.tensor([0, 0, 0])
+    T.assert_array_almost_equal(correlation(a, b, axis=1), res)
+
+
