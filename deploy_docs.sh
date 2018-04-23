bf8dac9fcd4ea31cb6a00c8a71273ee5a9a629b3
Jean Kossaifi <jean.kossaifi@gmail.com>
2018-04-23 21:34:58 +0100

    DOC: use conda for install

--- deploy_docs.sh
+++ deploy_docs.sh
@@ -6,18 +6,21 @@
     git config --global user.email "admin@tensorly.org"
     git config --global user.name "Travis"
 
-	pip install slimit rcssmin matplotlib sphinx
+	# Installed the dependencies
+	conda install matplotlib Pillow sphinx
+	pip install slimit rcssmin 
+
+	# cd to the doc folder and build the doc
 	cd doc
 	make html
 	cd ..
 
     git clone --quiet https://github.com/tensorly/tensorly.github.io doc_folder
     cd doc_folder
-
     git rm -r dev/*
-    cp -r ../doc/_build/html dev/
-    git add dev
+    cp -r ../doc/_build/html/* dev/
 
-    git commit -m "Auto-update"
-    git push --force --quiet "https://${gh_token}@github.com/tensorly/tensorly.github.io" > /dev/null 2>&1
+    git add dev
+    git commit -m "Travis auto-update"
+    git push --force --quiet "https://github.com/tensorly/tensorly.github.io" > /dev/null 2>&1
 )
