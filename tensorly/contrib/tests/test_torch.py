1b8e97b23cf4c72e8f8d605a4dd14720596abc55
Scott Sievert <github@stsievert.com>
2018-07-17 15:01:50 -0500

    MAINT: travis

--- tensorly/contrib/tests/test_torch.py
+++ tensorly/contrib/tests/test_torch.py
@@ -1,16 +1,14 @@
-import torch
 import pytest
+import tensorly as tl
 from ... import contrib
 from ... import set_backend, get_backend
 
 
-def test_pytorch_model_raises():
-    assert get_backend() == 'numpy'
-    with pytest.raises(ValueError, match="set_backend"):
-        contrib.pytorch_model(model='resnet18')
-
-
 def test_pytorch_model():
-    set_backend('pytorch')
-    with pytest.raises(AttributeError, match="no attribute 'junk'"):
-        contrib.pytorch_model(model='junk')
+    if tl._BACKEND == 'pytorch':
+        import torch
+        with pytest.raises(AttributeError, match="no attribute 'junk'"):
+            contrib.pytorch_model(model='junk')
+    else:
+        with pytest.raises(ValueError, match="set_backend"):
+            contrib.pytorch_model(model='resnet18')
