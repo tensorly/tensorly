b530caa2e1c0a2f3aeb5c69a27852060b0195c44
Scott Sievert <github@stsievert.com>
2018-07-17 14:44:18 -0500

    MAINT: speed up tests

--- tensorly/contrib/tests/test_torch.py
+++ tensorly/contrib/tests/test_torch.py
@@ -12,22 +12,5 @@ def test_pytorch_model_raises():
 
 def test_pytorch_model():
     set_backend('pytorch')
-    params = contrib.pytorch_model(model='resnet18')
-    shapes = [p.shape for p in params]
-    set(shapes) == {torch.Size([64, 3, 7, 7]),
-                    torch.Size([64]),
-                    torch.Size([64, 64, 3, 3]),
-                    torch.Size([128, 64, 3, 3]),
-                    torch.Size([128, 64, 1, 1]),
-                    torch.Size([128, 128, 3, 3]),
-                    torch.Size([256, 128, 3, 3]),
-                    torch.Size([256]),
-                    torch.Size([128]),
-                    torch.Size([256, 256, 3, 3]),
-                    torch.Size([256, 128, 1, 1]),
-                    torch.Size([512]),
-                    torch.Size([1000, 512]),
-                    torch.Size([1000]),
-                    torch.Size([512, 256, 3, 3]),
-                    torch.Size([512, 512, 3, 3]),
-                    torch.Size([512, 256, 1, 1])}
+    with pytest.raises(AttributeError, match="no attribute 'junk'"):
+        contrib.pytorch_model(model='junk')
