ce48e52123f29c8fc0bd8a0b3d378ae1e54f7447
Aaron Meurer <asmeurer@gmail.com>
2018-12-06 11:53:58 -0700

    Make sure the dot product is against a n x 1 vector
    Only NumPy will take the dot product of a shape (m, n) with shape (n,) array it seems.

--- tensorly/decomposition/candecomp_parafac.py
+++ tensorly/decomposition/candecomp_parafac.py
@@ -194,7 +194,8 @@ def parafac(tensor, rank, n_iter_max=100, init='svd', svd='numpy_svd', tol=1e-8,
                 for i, f in enumerate(factors):
                     if i == mode:
                         continue
-                    partial_factor = tl.dot(partial_factor, f[:,r])
+                    partial_factor = tl.dot(partial_factor, tl.reshape(f[:,r], (f[:,r].shape[0], 1)))
+                    partial_factor = tl.reshape(partial_factor, partial_factor.shape[:-1])
                 mttkrp_parts.append(partial_factor)
             mttkrp = tl.stack(mttkrp_parts, axis=1)
             factor = tl.transpose(tl.solve(tl.transpose(pseudo_inverse), tl.transpose(mttkrp)))
