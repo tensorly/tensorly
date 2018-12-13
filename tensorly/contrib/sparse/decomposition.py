3c8fec98f62605cdfac9478882eb3dd810a0ec40
Hameer Abbasi <hameerabbasi@yahoo.com>
2018-12-13 12:49:53 +0100

    Wrap non-negative PARAFAC for the sparse back-end.

--- tensorly/contrib/sparse/decomposition.py
+++ tensorly/contrib/sparse/decomposition.py
@@ -1,7 +1,8 @@
-from ...decomposition import tucker, partial_tucker, non_negative_tucker, parafac
+from ...decomposition import tucker, partial_tucker, non_negative_tucker, parafac, non_negative_parafac
 from .core import wrap
 
 tucker = wrap(tucker)
 partial_tucker = wrap(partial_tucker)
 non_negative_tucker = wrap(non_negative_tucker)
 parafac = wrap(parafac)
+non_negative_parafac = wrap(non_negative_parafac)
