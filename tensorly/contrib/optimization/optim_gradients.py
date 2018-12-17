eb17114d3d2caedf1e7a32fca3588ddbd339419c
COHEN Jeremy <jeremy.cohen@irisa.fr>
2018-12-17 16:35:37 +0100

    more

---
+++ tensorly/contrib/optimization/optim_gradients.py
@@ -0,0 +1,98 @@
+import tensorly as tl
+import numpy as np
+import math
+from tensorly.base import unfold
+from tensorly.tenalg import khatri_rao
+
+# Author : Jeremy Cohen
+
+
+def fast_gradient_step(input_tensor, input_factors,
+                       rank, norm_tensor,
+                       input_aux_factors,
+                       step = 0.1, alpha = 0.5,
+                       qstep = 0,
+                       fixed_modes=None,
+                       weights=None, constraints=None):
+    """ One step of fast gradient update along all modes
+
+    Updates the factor using one step of a Fast gradient descent algorithm.
+    Constraints are imposed using a projection on the feasible set after the
+    gradient update of the auxilliary variables. This is formally very similar
+    to a gradient descent with extrapolation but practically faster.
+
+    Reference: Yuri Nesterov, Introductory lectures on convex optimization: A
+    basic course , vol. 87, Springer Science & Business Media, 2004.
+
+    Parameters
+    ----------
+    input_tensor : tensor of arbitrary order.
+    input_factors : current estimates for the PARAFAC decomposition of
+    input_tensor. The value of input_factor[update_mode] will be updated using
+    a least squares update.
+    rank : rank of the decomposition.
+    norm_tensor : the Frobenius norm of the input tensor
+    step : size of the update step for the gradient. If possible, choose 1/L
+           where L is the Lipschitz constant.
+    fixed_modes : indexes of modes that are not updated
+    weights : entry-wise weights for each data point, contained in a tensor.
+    constraints : a list containing constraints codes (see class Parafac).
+
+    Returns -------
+    out_factors : updated inputs factors
+    out_factors_aux : auxilliary factors, necessary for looping
+    alpha : updated value of averaging variable
+    rec_error : residual error after the ALS steps.
+
+    TODO :
+    Handle missing data
+    Handle personal projection operator
+    """
+    # Generating the mode update sequence
+    gen = (mode for mode in range(tl.ndim(input_tensor)) if mode not in fixed_modes)
+
+    # Initialization of factors and aux_factors
+    factors = input_factors
+    aux_factors = input_aux_factors
+
+    # Computing the gradient for updated modes:
+    for mode in gen:
+
+        # Unfolding
+        # TODO: precompute unfoldings
+        unfoldY = unfold(input_tensor,mode)
+
+        # Computing Hadamard of cross-products
+        cross = tl.tensor(np.ones((rank, rank)), **tl.context(input_tensor))
+        for i, factor in enumerate(input_factors):
+            if i != mode:
+                cross = cross*tl.dot(tl.transpose(aux_factor),aux_factor)
+
+        # Computing the Khatri Rao product
+        krao = khatri_rao(aux_factors,skip_matrix=mode)
+        rhs = tl.dot(unfoldY,krao)
+
+        # Compute the gradient for given mode
+        grad[mode] = rhs - tl.dot(aux_factors[mode],cross)
+       
+    # alpha, beta variables update
+    alpha_new = 1/2*(qstep-alpha**2 + math.sqrt(
+        (qstep-alpha**2)**2 + 4*alpha**2))
+    beta = alpha*(1-alpha)/((alpha**2)*(1-alpha_new))
+
+    # Updates and Extrapolation
+    for mode in gen:
+        # Gradient step
+        factors[mode] = factors_aux - step*grad[mode] 
+        # >>>
+
+    # error computation (improved using precomputed quantities)
+    rec_error = norm_tensor ** 2 - 2*tl.dot(
+            tl.tensor_to_vec(input_factors[mode]),tl.tensor_to_vec(
+                rhs)) + tl.norm(tl.dot(input_factors[mode],tl.transpose(krao)),2)**2
+    rec_error = rec_error ** (1/2) / norm_tensor
+
+    # outputs
+    return input_factors, rec_error
+
+
