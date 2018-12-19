3cbf815ef531ca8008674e1f12b3ee99080063b3
Aaron Meurer <asmeurer@gmail.com>
2018-12-19 14:41:27 -0700

    Fix the PDF build of the docs
    The LaTeX preamble wasn't properly included in the latex_elements dictionary
    in conf.py.

--- doc/conf.py
+++ doc/conf.py
@@ -241,20 +241,6 @@ htmlhelp_basename = 'tensorlydoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
-latex_elements = {
-# The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
-
-# The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
-
-# Additional stuff for the LaTeX preamble.
-#'preamble': '',
-
-# Latex figure (float) alignment
-#'figure_align': 'htbp',
-}
-
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
@@ -295,10 +281,11 @@ trim_doctests_flags = True
 # Documents to append as an appendix to all manuals.
 #latex_appendices = []
 latex_elements = {
-  'classoptions': ',oneside',
-  'babel': '\\usepackage[english]{babel}',
-  # Get completely rid of index
-  'printindex': '',
+    'classoptions': ',oneside',
+    'babel': '\\usepackage[english]{babel}',
+    # Get completely rid of index
+    'printindex': '',
+    'preamble': latex_preamble,
 }
 
 # If false, no module index is generated.
