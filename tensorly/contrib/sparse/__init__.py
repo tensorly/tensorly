a1b8dc73ba3199a8d0682cecfaa67a0ddd674228
Aaron Meurer <asmeurer@gmail.com>
2019-02-18 15:40:11 -0700

    Fix sparse backend imports

--- tensorly/contrib/sparse/__init__.py
+++ tensorly/contrib/sparse/__init__.py
@@ -1,9 +1,11 @@
-from ...backend import (set_backend, get_backend, override_module_dispatch,
-                        tensor, is_tensor, context, shape, ndim, to_numpy,
-                        copy, concatenate, reshape, moveaxis, transpose, arange, ones,
-                        zeros, zeros_like, eye, clip, where, max, min, all, mean,
-                        sum, prod, sign, abs, sqrt, norm, dot, kron, kr, solve, qr,
-                        partial_svd)
+from ...backend import set_backend, get_backend, override_module_dispatch
+
+from .backend import (tensor, is_tensor, context, shape, ndim, to_numpy, copy,
+                      concatenate, reshape, moveaxis, transpose,
+                      arange, ones, zeros, zeros_like, eye,
+                      clip, where, max, min, all, mean, sum,
+                      prod, sign, abs, sqrt, norm, dot, kron,
+                      kr, solve, qr, partial_svd)
 
 from .core import unfold
 
