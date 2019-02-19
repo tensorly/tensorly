a92dd4bb9fc4745ec0680a39574810598ca690d5
Aaron Meurer <asmeurer@gmail.com>
2019-02-18 20:26:37 -0700

    Add a test for sparse partial_tucker

--- tensorly/contrib/sparse/tests/test_sparse.py
+++ tensorly/contrib/sparse/tests/test_sparse.py
@@ -20,3 +20,16 @@ def test_sparse_backend():
     tensor2 = tl.dot(tensor, tl.tensor([[-1], [2]]))
     assert isinstance(tensor2, sparse.COO)
     np.testing.assert_equal(tensor2, tl.tensor([[3], [5]]))
+
+def test_sparse_partial_tucker():
+    # Make sure the algorithm stays sparse. This will run out of memory on
+    # most machines if the algorithm densifies.
+    random_state = 1234
+    rank = 3
+    core = sparse.random((2482, rank, rank), random_state=random_state)
+    factors = [sparse.random((2862, rank), random_state=random_state),
+               sparse.random((14036, rank), random_state=random_state)]
+
+    tensor = tl.tenalg.multi_mode_dot(core, factors, [1, 2])
+    new_core, new_factors = partial_tucker(tensor, modes=[1, 2], rank=[rank, rank],
+        init='random', tol=1e-3, random_state=random_state)
