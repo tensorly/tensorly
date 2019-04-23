71fd23b869fd7f0ecb9a28f24beed10616c2e449
Jean Kossaifi <jean.kossaifi@gmail.com>
2019-04-23 17:23:56 +0200

    Added MPS tensor validation

--- tensorly/mps_tensor.py
+++ tensorly/mps_tensor.py
@@ -5,6 +5,50 @@ Core operations on tensors in Matrix Product State (MPS) format, also known as T
 import tensorly as tl
 
 
+def _validate_mps_tensor(mps_tensor):
+    factors = mps_tensor
+    n_factors = len(factors)
+    
+    if n_factors < 2:
+        raise ValueError('A Matrix-Product-State (ttrain) tensor should be composed of at least two factors and a core.'
+                         'However, {} factor was given.'.format(n_factors))
+
+    rank = []
+    shape = []
+    for index, factor in enumerate(factors):
+        current_rank, current_shape, next_rank = tl.shape(factor)
+
+        # Check that factors are third order tensors
+        if not tl.ndim(factor)==3:
+            raise ValueError('MPS expresses a tensor as third order factors (tt-cores).\n'
+                             'However, tl.ndim(factors[{}]) = {}'.format(
+                                 index, tl.ndim(factor)))
+        # Consecutive factors should have matching ranks
+        if index and tl.shape(factors[index - 1])[2] != current_rank:
+            raise ValueError('Consecutive factors should have matchine ranks\n'
+                             ' -- e.g. tl.shape(factors[0])[2]) == tl.shape(factors[1])[0])\n'
+                             'However, tl.shape(factor[{}])[2] == {} but'
+                             ' tl.shape(factor[{}])[0] == {} '.format(
+                              index - 1, tl.shape(factors[index - 1])[2], index, current_rank))
+        # Check for boundary conditions
+        if (index == 0) and current_rank != 1:
+            raise ValueError('Boundary conditions dictate factor[0].shape[0] == 1.'
+                             'However, got factor[0].shape[0] = {}.'.format(
+                              current_rank))
+        if (index == n_factors - 1) and next_rank != 1:
+            raise ValueError('Boundary conditions dictate factor[-1].shape[2] == 1.'
+                             'However, got factor[{}].shape[2] = {}.'.format(
+                              n_factors, next_rank))
+    
+        shape.append(current_shape)
+        rank.append(current_rank)
+        
+    # Add last rank (boundary condition)
+    rank.append(next_rank)
+        
+    return tuple(shape), tuple(rank)
+
+
 def mps_to_tensor(factors):
     """Returns the full tensor whose MPS decomposition is given by 'factors'
 
