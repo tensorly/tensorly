dc0a86c38be09cfa7e453e33a0d5adb50a42369c
Cor3Down <cor3down@protonmail.ch>
2019-09-09 10:24:02 +0430

    Revert "refactor: add static method decorator"
    This reverts commit 58bb5d179d129f96e86660a9e0f0ee478fc36e63.

--- tensorly/backend/pytorch_backend.py
+++ tensorly/backend/pytorch_backend.py
@@ -200,7 +200,6 @@ class PyTorchBackend(Backend):
         indices = torch.arange(tensor.shape[axis] - 1, -1, -1, dtype=torch.int64)
         return tensor.index_select(axis, indices)
 
-    @staticmethod
     def truncated_svd(self, matrix, n_eigenvecs=None):
         """Computes a truncated SVD on `matrix` using pytorch's SVD
 
