fdb24f477b443554143e7d0707bdfc8b0694177f
ELNS <57490926+EverLookNeverSee@users.noreply.github.com>
2020-03-11 19:10:39 +0330

    Fixing Some Minor Issues (#145)
    Cosmetic changes
    
        unused imports & parameter, redundant parenthesis

--- tensorly/decomposition/robust_decomposition.py
+++ tensorly/decomposition/robust_decomposition.py
@@ -1,9 +1,8 @@
 import numpy as np
-from ..base import fold, unfold
-from ..tenalg import khatri_rao
-from ..random import check_random_state
-from ..tenalg.proximal import soft_thresholding, svd_thresholding
 from .. import backend as T
+from ..base import fold, unfold
+from ..tenalg.proximal import soft_thresholding, svd_thresholding
+
 
 # Author: Jean Kossaifi
 
@@ -12,7 +11,7 @@ from .. import backend as T
 
 def robust_pca(X, mask=None, tol=10e-7, reg_E=1, reg_J=1,
                mu_init=10e-5, mu_max=10e9, learning_rate=1.1,
-               n_iter_max=100, random_state=None, verbose=1):
+               n_iter_max=100, verbose=1):
     """Robust Tensor PCA via ALM with support for missing values
 
         Decomposes a tensor `X` into the sum of a low-rank component `D`
@@ -39,7 +38,6 @@ def robust_pca(X, mask=None, tol=10e-7, reg_E=1, reg_J=1,
         percentage increase of mu at each iteration
     n_iter_max : int, optional, default is 100
         maximum number of iteration
-    random_state : None, int or RandomState, optional, default is None
     verbose : int, default is 1
         level of verbosity
 
@@ -113,7 +111,7 @@ def robust_pca(X, mask=None, tol=10e-7, reg_E=1, reg_J=1,
 
         # Convergence check
         if iteration > 1:
-            if (max(rec_X[-1], rec_D[-1]) <= tol):
+            if max(rec_X[-1], rec_D[-1]) <= tol:
                 if verbose:
                     print('\nConverged in {} iterations'.format(iteration))
                 break
