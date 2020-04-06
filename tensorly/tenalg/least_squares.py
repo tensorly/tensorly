583f410153b72094c7f072510e164e90acbb2ad5
Isabell Lehmann <ilehmann@mail.upb.de>
2020-04-06 11:45:02 +0200

    change @ to dot

--- tensorly/tenalg/least_squares.py
+++ tensorly/tenalg/least_squares.py
@@ -3,5 +3,5 @@ from .. import backend as T
 
 def solve_least_squares(A, B):
     # solve ||B-AX||^2 (AX = B -> X = A^+ @ B), with A^+: pseudo inverse
-    X = T.pinv(A) @ B
+    X = T.dot(T.pinv(A), B)
     return X
