330d990c22f88783285abcfbc3afd0b3b7d3f2be
aman <amanj120@gmail.com>
2020-04-29 18:01:42 -0400

    sparse cpd implemented
    commit 2
    
    fixed the testing frameowrk to be faster, now, after importing tensorly, running the command tensorly.contrib.sparse.tests.test_decomposition.test_tf_sparse_cpd() should perform a quick test of the cp decomposition function
    
    All the necessary functions for CP Decomposition on tf.sparse.SparseTensor are added. I need some help on how to rebase this code to fit the tensorly guidelines

---
+++ tensorly/contrib/sparse/backend/tensorflow_backend.py
@@ -0,0 +1,116 @@
+import numpy as np
+import tensorflow as tf
+from ....kruskal_tensor import KruskalTensor
+from ....backend.core import Backend
+
+def is_sparse(x):
+    return isinstance(x, tf.sparse.SparseTensor)
+
+class TensorflowBackend(Backend):
+    backend_name = 'tensorflow.sparse'
+
+    @staticmethod
+    def tensor(data, dtype=np.float32, device=None, device_id=None):
+        if isinstance(data, tf.sparse.SparseTensor):
+            return data
+
+
+def norm(tensor, order=2, axis=None):
+    if axis == ():
+        axis = None
+
+    values = tensor.values.numpy()
+    if order == 'inf':
+        return np.max(values, axis=axis)
+    if order == 1:
+        return np.sum(values, axis=axis)
+    elif order == 2:
+        return np.sqrt(np.sum(values ** 2, axis=axis))
+    else:
+        return np.sum(values**order, axis=axis) ** (1 / order)
+
+
+# This function is easy to decorate with Numba '@njit(parallel=True)' directive for ~10x improvement in performance
+def mttkrp(values, indices, factors, n, rank, dims):
+    output = np.zeros((dims[n], rank))
+    # if decorating with Numba '@njit(parallel=True)' directive, replcase range with numba.prange
+    for l in range(len(values)):
+        cur_index = indices[l]
+        prod = [values[l]] * rank  # makes the value into a row
+
+        for mode, cv in enumerate(cur_index):  # does elementwise row multiplications
+            if mode != n:
+                for r in range(rank):
+                    prod[r] *= factors[mode][cv][r]
+
+        for r in range(rank):
+            output[cur_index[n]][r] += prod[r]
+
+    return output
+
+
+def parafac(tensor, rank, n_iter_max=100, verbose=False):
+    """CANDECOMP/PARAFAC decomposition via alternating least squares (ALS)
+        Computes a rank-`rank` decomposition of `tensor` [1]_ such that,
+
+            ``tensor = [|weights; factors[0], ..., factors[-1] |]``.
+
+        Parameters
+        ----------
+        tensor : tf.sparse.SparseTensor
+        rank  : int
+            Number of components.
+        n_iter_max : int
+            Maximum number of iteration
+        verbose: bool
+            Wether or not to print progress at every iteration
+
+        Returns
+        -------
+        KruskalTensor : (weight, factors)
+            * weights : 1D array of shape (rank, )
+                all ones if normalize_factors is False (default),
+                weights of the (normalized) factors otherwise
+            * factors : List of factors of the CP decomposition element `i` is of shape
+                (tensor.shape[i], rank)
+
+        References
+        ----------
+        .. [1] T.G.Kolda and B.W.Bader, "Tensor Decompositions and Applications",
+           SIAM REVIEW, vol. 51, n. 3, pp. 455-500, 2009.
+        """
+
+    dims = tensor.shape.as_list()
+    nd = len(dims)
+    factors = [np.random.random((d, rank)) for d in dims]
+    weights = np.ones((1, rank))
+
+    for iteration in range(n_iter_max):
+        if verbose:
+            print("finished {} iterations".format(iteration+1), end="\r")
+        for n in range(nd):
+
+            # the following block calculates inverse of the hadamard product
+            h = np.matmul(weights.T, weights)
+            for i, f in enumerate(factors):
+                if i != n:
+                    h *= np.matmul(f.T, f)
+            vinv = np.linalg.pinv(h)
+
+            # the following block calculates An by doing MTTKRP and multiplying it by the inverse of the hadamard
+            vals = tensor.values.numpy()
+            idxs = tensor.indices.numpy()
+            mk = mttkrp(vals, idxs, factors, n, rank, dims)
+
+            wmk = np.multiply(mk, weights[0])  #multiply each column by the weights
+            An = np.matmul(wmk, vinv)          #nth factor
+
+            # the following block normalizes the columns and stored
+            weight = np.linalg.norm(An, axis=0)
+            b = np.where(weight < 1e-12, 1, weight)
+            weights[0] *= b #avoids dividing by small weights, reduces error
+            An /= b
+
+            factors[n] = An
+
+    return KruskalTensor((weights[0], factors))
--- tensorly/contrib/sparse/tests/test_decomposition.py
+++ tensorly/contrib/sparse/tests/test_decomposition.py
@@ -1,15 +1,18 @@
 from ..decomposition import parafac
 from ..tenalg import multi_mode_dot
 from ..kruskal_tensor import kruskal_to_tensor
-from .... import backend as tl 
+from ....kruskal_tensor import kruskal_norm
+from .... import backend as tl
+from scipy import sparse
+from ..backend import tensorflow_backend
 
 import pytest
 if not tl.get_backend() == "numpy":
     pytest.skip("Tests for sparse only with numpy backend", allow_module_level=True)
 pytest.importorskip("sparse")
 
-import sparse
 import numpy as np
+import tensorflow as tf
 
 def test_sparse_parafac():
     """Test for sparse parafac"""
@@ -23,3 +26,67 @@ def test_sparse_parafac():
     tensor = kruskal_to_tensor((weights, factors))
     _ = parafac(tensor, rank=rank, init='random', 
                 n_iter_max=1, random_state=random_state)
+
+
+def generate_random_sp_tensor(dimensions, d=0.0001):
+    nd = len(dimensions)
+    num_items = min(1000000, int(np.prod(dimensions) * d))
+
+    idxs2 = np.random.rand(num_items, nd)
+    idxs3 = np.trunc(idxs2 * dimensions).astype(int)
+    idxs4 = [tuple(i) for i in idxs3]
+
+    indices = list(set(idxs4))
+    values = np.random.rand(len(indices))
+    indices.sort()
+    st = tf.sparse.SparseTensor(indices=indices, values=values, dense_shape=dimensions)
+    return st
+
+
+def inner_product(kt, st):
+    s = 0.0
+    weights, factors = kt
+    idxs = st.indices.numpy()
+    vals = st.values.numpy()
+    for i, index in enumerate(idxs):
+        st_val = vals[i]
+        kt_val = weights
+        for fac_no, dim in enumerate(index):
+            kt_val = np.multiply(factors[fac_no][dim], kt_val)
+        s += (sum(kt_val) * st_val)
+    return s
+
+
+def fit(kt, st):
+    normX = tensorflow_backend.norm(st)
+    normP = kruskal_norm(kt)
+    ip = inner_product(kt, st)
+    return 1 - ((normX**2 + normP**2 - 2*ip)**0.5)/normX
+
+def test_tf_sparse_cpd():
+    print("generating tensor")
+    shape = (1000, 1000, 1000)
+    density = 0.00001
+    rank = 20
+
+    st = generate_random_sp_tensor(shape, d=density)
+    print("performing decomposition")
+    cpd = tensorflow_backend.parafac(st, rank, n_iter_max=50, verbose=True)
+    print("testing fit")
+    fit_st_rebuilt = fit(cpd, st)
+
+    result_text = '''
+    +--------------------------------------------
+    | shape: {}
+    | rank: {}
+    | iterations: 100
+    | density: {}
+    | actual density: {}
+    | number of non-zeros: {}
+    |--------------------------------------------
+    | fit: {}
+    +--------------------------------------------
+    '''.format(shape, rank, density, (st.values.shape[0] / np.prod(shape)), st.values.shape[0], fit_st_rebuilt)
+
+    print(result_text)
+
