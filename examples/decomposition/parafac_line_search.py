93ee05bdc187caeaf6fe1cd3667aee9d5de8c11d
Aaron Meyer <git@ameyer.me>
2020-07-10 10:36:45 -0700

    Some edits to polish the demo script.

--- examples/decomposition/parafac_line_search.py
+++ examples/decomposition/parafac_line_search.py
@@ -1,28 +1,39 @@
+"""
+Using line search with PARAFAC
+==========================================
+
+Example on how to use :func:`tensorly.decomposition.parafac` with line search to accelerate convergence.
+"""
+
+from time import time
+import numpy as np
 import tensorly as tl
 from tensorly.random import random_kruskal
-from time import time
 from tensorly.decomposition import parafac
-import numpy as np
 import matplotlib.pyplot as plt
 
-tol = np.logspace(-1, -10)
+tol = np.logspace(-1, -9)
 err = np.empty_like(tol)
 err_ls = np.empty_like(tol)
 tt = np.empty_like(tol)
 tt_ls = np.empty_like(tol)
-tensor = random_kruskal((10, 20, 20), 5, random_state=1234, full=True)
+tensor = random_kruskal((10, 10, 10), 3, random_state=1234, full=True)
 
-fac = parafac(tensor, rank=3, n_iter_max=2000000, tol=1.0e-14, orthogonalise=5, linesearch=True)
+# Get a high-accuracy decomposition for comparison
+fac = parafac(tensor, rank=3, n_iter_max=2000000, tol=1.0e-15, linesearch=True)
 err_min = tl.norm(tl.kruskal_to_tensor(fac) - tensor)
 
-for ii, toll in enumerate(tol), total=len(tol):
+for ii, toll in enumerate(tol):
+	# Run PARAFAC decomposition without line search and time
     start = time()
-    fac = parafac(tensor, rank=3, n_iter_max=2000000, orthogonalise=5, tol=toll)
+    fac = parafac(tensor, rank=3, n_iter_max=2000000, tol=toll)
     tt[ii] = time() - start
+    # Run PARAFAC decomposition with line search and time
     start = time()
-    fac_ls = parafac(tensor, rank=3, n_iter_max=2000000, orthogonalise=5, tol=toll, linesearch=True)
+    fac_ls = parafac(tensor, rank=3, n_iter_max=2000000, tol=toll, linesearch=True)
     tt_ls[ii] = time() - start
-    
+
+    # Calculate the error of both decompositions
     err[ii] = tl.norm(tl.kruskal_to_tensor(fac) - tensor)
     err_ls[ii] = tl.norm(tl.kruskal_to_tensor(fac_ls) - tensor)
 
