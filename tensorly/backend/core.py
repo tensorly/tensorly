Fix svd sign indeterminacy

tensorly/backend/core.py
@@ -18,6 +18,45 @@ class Index():
         return indices
 
 
+def svd_flip(u, v, u_based_decision=True):
+    """Sign correction to ensure deterministic output from SVD.
+    Adjusts the columns of u and the rows of v such that the loadings in the
+    columns in u that are largest in absolute value are always positive.
+    This function is borrowed from scikit-learn/utils/extmath.py
+    Parameters
+    ----------
+    u : ndarray
+        u and v are the output of `partial_svd`
+    v : ndarray
+        u and v are the output of `partial_svd`
+    u_based_decision : boolean, (default=True)
+        If True, use the columns of u as the basis for sign flipping.
+        Otherwise, use the rows of v. The choice of which variable to base the
+        decision on is generally algorithm dependent.
+    Returns
+    -------
+    u_adjusted, v_adjusted : arrays with the same dimensions as the input.
+    """
+    if u_based_decision:
+        # columns of u, rows of v
+        max_abs_cols = np.argmax(np.abs(u), axis=0)
+        signs = np.sign(u[max_abs_cols, range(u.shape[1])])
+        u = u * signs
+        if v.shape[0] > u.shape[1]:
+            signs = np.concatenate((signs, np.ones(v.shape[0] - u.shape[1])))
+        v = v * signs[:v.shape[0]][:, None]
+    else:
+        # rows of v, columns of u
+        max_abs_rows = np.argmax(np.abs(v), axis=1)
+        signs = np.sign(v[range(v.shape[0]), max_abs_rows])
+        v = v * signs[:, None]
+        if u.shape[1] > v.shape[0]:
+            signs = np.concatenate((signs, np.ones(u.shape[1] - v.shape[0])))
+        u = u * signs[:u.shape[1]]
+
+    return u, v
+
+
 class Backend(object):
     @classmethod
     def register_method(cls, name, func):
@@ -695,7 +734,7 @@ class Backend(object):
 
         return res*m
 
-    def partial_svd(self, matrix, n_eigenvecs=None, random_state=None, **kwargs):
+    def partial_svd(self, matrix, n_eigenvecs=None, flip=False, random_state=None, **kwargs):
         """Computes a fast partial SVD on `matrix`
 
         If `n_eigenvecs` is specified, sparse eigendecomposition is used on
@@ -707,6 +746,9 @@ class Backend(object):
             A 2D tensor.
         n_eigenvecs : int, optional, default is None
             If specified, number of eigen[vectors-values] to return.
+        flip : bool, default is False
+            If True, the SVD sign ambiguity is resolved by making the largest component
+            in the columns of U, positive.
         random_state: {None, int, np.random.RandomState}
             If specified, use it for sampling starting vector in a partial SVD(scipy.sparse.linalg.eigsh)
         **kwargs : optional
@@ -788,6 +830,9 @@ class Backend(object):
             # WARNING: here, V is still the transpose of what it should be
             U, S, V = U[:, ::-1], S[::-1], V[:, ::-1]
             V = V.T.conj()
+
+        if flip:
+            U, V = svd_flip(U, V)
 
         if not is_numpy:
             U = self.tensor(U, **ctx)