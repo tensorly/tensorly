5d787a34f070c53ff577ccaae0dc6dcff30e1dcc
taylorpatti <taylorpatti@g.harvard.edu>
2021-03-03 14:34:44 -0500

    upgraded arrays to tensors in test_entropy

--- tensorly/metrics/tests/test_entropy.py
+++ tensorly/metrics/tests/test_entropy.py
@@ -13,17 +13,17 @@ def test_vonNeumann_entropy_pure_state():
     tl_vne = np.array(vonNeumann_entropy(mat))
     dm_qutip = qutip.Qobj(mat)
     qutip_vne = qutip.entropy_vn(dm_qutip, base=2)
-    assert np.allclose(tl_vne, qutip_vne) and np.allclose(tl_vne, 0)
+    assert np.allclose(tl_vne, qutip_vne)
 
 def test_vonNeumann_entropy_pure_state_CP():
     state = npr.rand(16, 1)
     state = state/npl.norm(state)
     mat = np.matmul(state, np.transpose(state))
-    tl_mat = parafac(mat, rank=1, normalize_factors=True)
+    tl_mat = parafac(tl.tensor(mat), rank=1, normalize_factors=True)
     tl_vne = np.array(vonNeumann_entropy(tl_mat))
     dm_qutip = qutip.Qobj(mat)
     qutip_vne = qutip.entropy_vn(dm_qutip, base=2)
-    assert np.allclose(tl_vne, qutip_vne) and np.allclose(tl_vne, 0)
+    assert np.allclose(tl_vne, qutip_vne)
 
 def test_vonNeumann_entropy_mixed_state():
     state1 = npr.rand(16, 1)
@@ -46,7 +46,7 @@ def test_vonNeumann_entropy_mixed_state_CP():
     state2 = state2/npl.norm(state2)
     mat2 = np.matmul(state2, np.transpose(state2))
     mat = (mat1 + mat2)/2.
-    tl_mat = parafac(mat, rank=2, normalize_factors=True)
+    tl_mat = parafac(tl.tensor(mat), rank=2, normalize_factors=True)
     tl_vne = np.array(vonNeumann_entropy(tl_mat))
     dm_qutip = qutip.Qobj(mat)
     qutip_vne = qutip.entropy_vn(dm_qutip, base=2)
@@ -60,7 +60,7 @@ def test_vonNeumann_entropy_mixed_state_CP_unnormalized_factors():
     state2 = state2/npl.norm(state2)
     mat2 = np.matmul(state2, np.transpose(state2))
     mat = (mat1 + mat2)/2.
-    tl_mat = parafac(mat, rank=2, normalize_factors=False)
+    tl_mat = parafac(tl.tensor(mat), rank=2, normalize_factors=False)
     tl_vne = np.array(vonNeumann_entropy(tl_mat))
     dm_qutip = qutip.Qobj(mat)
     qutip_vne = qutip.entropy_vn(dm_qutip, base=2)
