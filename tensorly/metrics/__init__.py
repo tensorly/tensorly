57a6a4f5bd190d4f6aeb435bb6b783a77354551d
taylorpatti <taylorpatti@g.harvard.edu>
2021-03-03 12:18:07 -0500

    added dispatch and imports

--- tensorly/metrics/__init__.py
+++ tensorly/metrics/__init__.py
@@ -3,4 +3,5 @@ The :mod:`tensorly.metrics` module includes utilities to measure performance
 (e.g. regression error).
 """
 
-from .regression import RMSE, MSE 
+from .regression import RMSE, MSE
+from .entropy import vonNeumann_entropy
