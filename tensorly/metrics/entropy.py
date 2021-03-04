55ed12ebcce6cd71945fef8387767484c80abc2c
taylorpatti <taylorpatti@g.harvard.edu>
2021-03-04 07:11:19 -0500

    updated log2 backends, updated entropy

--- tensorly/metrics/entropy.py
+++ tensorly/metrics/entropy.py
@@ -1,4 +1,5 @@
 from .. import backend as T
+from ..cp_tensor import CPTensor
 from ..cp_tensor import cp_normalize
 import numpy as np
 
@@ -19,16 +20,14 @@ def vonNeumann_entropy(tensor, offset=1e-12):
     -------
     float
     """
-    if tensor.__class__.__name__ == 'CPTensor':
-        weights = tensor[0]
-        if len(weights) == 1:
-            return 0
-        if np.allclose(weights, np.ones(len(weights))):
-            weights = T.tensor(cp_normalize(tensor)[0])
-        return T.sum(-weights*T.log2(weights))
+    if isinstance(tensor, CPTensor):
+        eig_vals = cp_normalize(tensor).weights
 
-    tensor = T.tensor(tensor)
-    eig_vals = T.eigh(tensor)[0]
+    else:
+        eig_vals = T.eigh(tensor)[0]
+
     eig_vals = eig_vals[eig_vals > offset]
-    return -T.sum(T.log2(eig_vals)*eig_vals)
+
+
+    return float(-T.sum(T.log2(eig_vals) * eig_vals))
 
--- tensorly/metrics/tests/test_entropy.py
+++ tensorly/metrics/tests/test_entropy.py
@@ -5,39 +5,15 @@ import tensorly as tl
 from ..entropy import vonNeumann_entropy
 from ...decomposition import parafac
 
-mat_pure = tl.tensor([[3.53447351e-03, 3.30106160e-02, 1.88930958e-02, 6.31659996e-04,
-        2.13221095e-02, 1.88003714e-02, 6.27753920e-03, 3.50348164e-02],
-       [3.30106160e-02, 3.08306391e-01, 1.76454210e-01, 5.89946014e-03,
-        1.99140259e-01, 1.75588200e-01, 5.86297890e-02, 3.27211639e-01],
-       [1.88930958e-02, 1.76454210e-01, 1.00990733e-01, 3.37646124e-03,
-        1.13974728e-01, 1.00495085e-01, 3.35558179e-02, 1.87274326e-01],
-       [6.31659996e-04, 5.89946014e-03, 3.37646124e-03, 1.12886502e-04,
-        3.81056006e-03, 3.35989008e-03, 1.12188431e-03, 6.26121314e-03],
-       [2.13221095e-02, 1.99140259e-01, 1.13974728e-01, 3.81056006e-03,
-        1.28628027e-01, 1.13415357e-01, 3.78699621e-02, 2.11351475e-01],
-       [1.88003714e-02, 1.75588200e-01, 1.00495085e-01, 3.35989008e-03,
-        1.13415357e-01, 1.00001871e-01, 3.33911310e-02, 1.86355211e-01],
-       [6.27753920e-03, 5.86297890e-02, 3.35558179e-02, 1.12188431e-03,
-        3.78699621e-02, 3.33911310e-02, 1.11494677e-02, 6.22249488e-02],
-       [3.50348164e-02, 3.27211639e-01, 1.87274326e-01, 6.26121314e-03,
-        2.11351475e-01, 1.86355211e-01, 6.22249488e-02, 3.47276152e-01]])
+state = npr.rand(8, 1).astype(np.float32)
+state = state/npl.norm(state)
+mat_pure = tl.tensor(np.matmul(state, np.transpose(state)))
 
-mat_mixed = tl.tensor([[0.05764554, 0.07271075, 0.05774785, 0.08440747, 0.01953676,
-        0.02577351, 0.09599539, 0.07482428],
-       [0.07271075, 0.2106877 , 0.13681119, 0.08753369, 0.1084978 ,
-        0.10426328, 0.12271515, 0.2212482 ],
-       [0.05774785, 0.13681119, 0.0922471 , 0.07437719, 0.06465954,
-        0.06440067, 0.09704334, 0.14317333],
-       [0.08440747, 0.08753369, 0.07437719, 0.12660652, 0.0152624 ,
-        0.02632028, 0.14030151, 0.08937212],
-       [0.01953676, 0.1084978 , 0.06465954, 0.0152624 , 0.06572383,
-        0.05930842, 0.03368433, 0.11477836],
-       [0.02577351, 0.10426328, 0.06440067, 0.02632028, 0.05930842,
-        0.05479861, 0.04390419, 0.10996953],
-       [0.09599539, 0.12271515, 0.09704334, 0.14030151, 0.03368433,
-        0.04390419, 0.15988065, 0.12634305],
-       [0.07482428, 0.2212482 , 0.14317333, 0.08937212, 0.11477836,
-        0.10996953, 0.12634305, 0.23241005]])
+state = np.array([[0.03004805, 0.42426117, 0.5483771 , 0.4784077 , 0.25792725, 0.34388784, 0.99927586, 0.96605812]])
+state = state/npl.norm(state)
+state2 = np.array([[0.84250089, 0.43429687, 0.26551928, 0.18262211, 0.55584835, 0.2565509 , 0.33197401, 0.97741178]])
+state2 = state2/npl.norm(state2)
+mat_mixed = tl.tensor((np.matmul(state.T, state) + np.matmul(state2.T, state2))/2.)
 
 def test_vonNeumann_entropy_pure_state():
     tl_vne = np.array(vonNeumann_entropy(mat_pure))
@@ -50,14 +26,15 @@ def test_vonNeumann_entropy_pure_state_CP():
 
 def test_vonNeumann_entropy_mixed_state():
     tl_vne = np.array(vonNeumann_entropy(mat_mixed))
-    assert np.allclose(tl_vne, np.array([0.7072100566602336]), rtol=1e-03, atol=1e-03)
+    print(tl_vne)
+    assert np.allclose(tl_vne, np.array([0.5545]), rtol=1e-03, atol=1e-03)
 
 def test_vonNeumann_entropy_mixed_state_CP():
     mat = parafac(tl.tensor(mat_mixed), rank=2, normalize_factors=True)
     tl_vne = np.array(vonNeumann_entropy(mat))
-    assert np.allclose(tl_vne, np.array([0.7072100566602336]), rtol=1e-03, atol=1e-03)
+    assert np.allclose(tl_vne, np.array([0.5545]), rtol=1e-03, atol=1e-03)
 
 def test_vonNeumann_entropy_mixed_state_CP_unnormalized_factors():
     mat = parafac(tl.tensor(mat_mixed), rank=2, normalize_factors=True)
     tl_vne = np.array(vonNeumann_entropy(mat))
-    assert np.allclose(tl_vne, np.array([0.7072100566602336]), rtol=1e-03, atol=1e-03)
+    assert np.allclose(tl_vne, np.array([0.5545]), rtol=1e-03, atol=1e-03)
