8c271d54da8f609c9b56e9d0ab98d453b3e4f81c
taylorpatti <taylorpatti@g.harvard.edu>
2021-03-08 07:23:17 -0500

    Updated entropy, added tt functions

--- tensorly/metrics/entropy.py
+++ tensorly/metrics/entropy.py
@@ -1,17 +1,17 @@
+import tensorly as tl
 from .. import backend as T
-from ..cp_tensor import CPTensor
-from ..cp_tensor import cp_normalize
-import numpy as np
+from ..cp_tensor import CPTensor, cp_normalize
+from ..tt_tensor import tt_to_tensor
 
 # Author: Taylor Lee Patti <taylorpatti@g.harvard.edu>
 
 
-def vonNeumann_entropy(tensor, offset=1e-12):
+def vonneumann_entropy(tensor):
     """Returns the von Neumann entropy of the tensor.
 
     Parameters
     ----------
-    tensor : (matrix or CP tensor)
+    tensor : (matrix)
         Data structure
     offset : (float)
         Minimum value at which to not consider eigenvalue zero to avoid singularities.
@@ -20,14 +20,78 @@ def vonNeumann_entropy(tensor, offset=1e-12):
     -------
     float
     """
-    if isinstance(tensor, CPTensor):
-        eig_vals = cp_normalize(tensor).weights
+    eig_vals = T.eigh(tensor)[0]
+    eps = tl.eps(eig_vals.dtype)
+    eig_vals = eig_vals[eig_vals >  eps]
 
-    else:
-        eig_vals = T.eigh(tensor)[0]
+    return -T.sum(T.log2(eig_vals) * eig_vals)
 
-    eig_vals = eig_vals[eig_vals > offset]
+def tt_mps_entanglement_entropy(tensor, boundary):
+    """Returns the entanglement entropy of an MPS in TT tensor form. Assumes a traditional and
+    single MPS, that is, a linear pure state.
+
+    Parameters
+    ----------
+    tensor : (TT tensor)
+        Data structure
+    boundary : (int)
+        Qubit at which to partition system.
+    offset : (float)
+        Minimum value at which to not consider eigenvalue zero to avoid singularities.
+
+    Returns
+    -------
+    float
+    """
+    partial_mps = tensor[boundary]
+    dims = partial_mps.shape
+    partial_mps = tl.reshape(partial_mps, (1, dims[0]*dims[1], dims[2]))
+    partial_mps = tt_to_tensor([partial_mps] + tensor[boundary+1::])
+    partial_mps = tl.reshape(partial_mps, (dims[0]*dims[1], -1))
+    _, eig_vals, _ = tl.partial_svd(partial_mps, min(partial_mps.shape))
+    eig_vals = eig_vals**2
+    eps = tl.eps(eig_vals.dtype)
+    eig_vals = eig_vals[eig_vals > eps]
+
+    return -T.sum(T.log2(eig_vals) * eig_vals)
 
 
-    return float(-T.sum(T.log2(eig_vals) * eig_vals))
+def tt_vonneumann_entropy(tensor):
+    """Returns the von Neumann entropy of the TT tensor.
 
+    Parameters
+    ----------
+    tensor : (TT tensor)
+        Data structure
+    offset : (float)
+        Minimum value at which to not consider eigenvalue zero to avoid singularities.
+
+    Returns
+    -------
+    float
+    """
+    square_dim = int(tl.sqrt(tl.prod(tl.tensor(tensor.shape))))
+    tensor = tl.reshape(tt_to_tensor(tensor), (square_dim, square_dim))
+
+    return vonneumann_entropy(tensor)
+
+
+def cp_vonneumann_entropy(tensor):
+    """Returns the von Neumann entropy of the CP tensor.
+
+    Parameters
+    ----------
+    tensor : (CP tensor)
+        Data structure
+    offset : (float)
+        Minimum value at which to not consider eigenvalue zero to avoid singularities.
+
+    Returns
+    -------
+    float
+    """
+    eig_vals = cp_normalize(tensor).weights
+    eps = tl.eps(eig_vals.dtype)
+    eig_vals = eig_vals[eig_vals > eps]
+
+    return -T.sum(T.log2(eig_vals) * eig_vals)
--- tensorly/metrics/tests/test_entropy.py
+++ tensorly/metrics/tests/test_entropy.py
@@ -1,40 +1,69 @@
-import numpy as np
-import numpy.random as npr
-import numpy.linalg as npl
 import tensorly as tl
-from ..entropy import vonNeumann_entropy
-from ...decomposition import parafac
+from ..entropy import vonneumann_entropy, tt_mps_entanglement_entropy
+from ..entropy import tt_vonneumann_entropy, cp_vonneumann_entropy
+from ...decomposition import parafac, matrix_product_state
 
-state = npr.rand(8, 1).astype(np.float32)
-state = state/npl.norm(state)
-mat_pure = tl.tensor(np.matmul(state, np.transpose(state)))
-
-state = np.array([[0.03004805, 0.42426117, 0.5483771 , 0.4784077 , 0.25792725, 0.34388784, 0.99927586, 0.96605812]])
-state = state/npl.norm(state)
-state2 = np.array([[0.84250089, 0.43429687, 0.26551928, 0.18262211, 0.55584835, 0.2565509 , 0.33197401, 0.97741178]])
-state2 = state2/npl.norm(state2)
-mat_mixed = tl.tensor((np.matmul(state.T, state) + np.matmul(state2.T, state2))/2.)
+state1 = tl.tensor([[0.03004805, 0.42426117, 0.5483771 , 0.4784077 , 0.25792725, 0.34388784, 0.99927586, 0.96605812]])
+state1 = state1/tl.norm(state1)
+state2 = tl.tensor([[0.84250089, 0.43429687, 0.26551928, 0.18262211, 0.55584835, 0.2565509 , 0.33197401, 0.97741178]])
+state2 = state2/tl.norm(state2)
+mat_mixed = tl.tensor((tl.dot(tl.transpose(state1), state1) + tl.dot(tl.transpose(state2), state2))/2.)
+actual_vne = 0.5546
 
 def test_vonNeumann_entropy_pure_state():
-    tl_vne = np.array(vonNeumann_entropy(mat_pure))
-    assert np.allclose(tl_vne, np.array([0]), rtol=1e-03, atol=1e-03)
+    state = tl.randn((8, 1))
+    state = state/tl.norm(state)
+    mat_pure = tl.dot(state, tl.transpose(state))
+    tl_vne = vonneumann_entropy(mat_pure)
+    tl.testing.assert_array_almost_equal(tl_vne, 0, decimal=3)
+
+def test_tt_vonNeumann_entropy_pure_state():
+    state = tl.randn((8, 1))
+    state = state/tl.norm(state)
+    mat_pure = tl.reshape(tl.dot(state, tl.transpose(state)), (2, 2, 2, 2, 2, 2))
+    mat_pure = matrix_product_state(mat_pure, rank=[1, 2, 2, 2, 2, 2, 1])
+    tl_vne = tt_vonneumann_entropy(mat_pure)
+    tl.testing.assert_array_almost_equal(tl_vne, 0, decimal=3)
 
 def test_vonNeumann_entropy_pure_state_CP():
-    mat = parafac(tl.tensor(mat_pure), rank=1, normalize_factors=True)
-    tl_vne = np.array(vonNeumann_entropy(mat))
-    assert np.allclose(tl_vne, np.array([0]), rtol=1e-03, atol=1e-03)
+    state = tl.randn((8, 1))
+    state = state/tl.norm(state)
+    mat_pure = tl.dot(state, tl.transpose(state))
+    mat = parafac(mat_pure, rank=1, normalize_factors=True)
+    tl_vne = cp_vonneumann_entropy(mat)
+    tl.testing.assert_array_almost_equal(tl_vne, 0, decimal=3)
 
 def test_vonNeumann_entropy_mixed_state():
-    tl_vne = np.array(vonNeumann_entropy(mat_mixed))
-    print(tl_vne)
-    assert np.allclose(tl_vne, np.array([0.5545]), rtol=1e-03, atol=1e-03)
+    tl_vne = vonneumann_entropy(mat_mixed)
+    tl.testing.assert_array_almost_equal(tl_vne, actual_vne, decimal=3)
+
+def test_tt_vonNeumann_entropy_mixed_state():
+    tt_mixed = tl.reshape(mat_mixed, (2, 2, 2, 2, 2, 2))
+    tt_mixed = matrix_product_state(tt_mixed, rank=[1, 2, 4, 8, 4, 2, 1])
+    tl_vne = tt_vonneumann_entropy(tt_mixed)
+    tl.testing.assert_array_almost_equal(tl_vne, actual_vne, decimal=3)
 
 def test_vonNeumann_entropy_mixed_state_CP():
     mat = parafac(tl.tensor(mat_mixed), rank=2, normalize_factors=True)
-    tl_vne = np.array(vonNeumann_entropy(mat))
-    assert np.allclose(tl_vne, np.array([0.5545]), rtol=1e-03, atol=1e-03)
+    tl_vne = cp_vonneumann_entropy(mat)
+    tl.testing.assert_array_almost_equal(tl_vne, actual_vne, decimal=3)
 
 def test_vonNeumann_entropy_mixed_state_CP_unnormalized_factors():
-    mat = parafac(tl.tensor(mat_mixed), rank=2, normalize_factors=True)
-    tl_vne = np.array(vonNeumann_entropy(mat))
-    assert np.allclose(tl_vne, np.array([0.5545]), rtol=1e-03, atol=1e-03)
+    mat = parafac(tl.tensor(mat_mixed), rank=2, normalize_factors=False)
+    tl_vne = cp_vonneumann_entropy(mat)
+    tl.testing.assert_array_almost_equal(tl_vne, actual_vne, decimal=3)
+
+def test_tt_mps_entanglement_entropy_product_state():
+    mps = tl.tensor([1,0,0,0,0,0,0,0])
+    mps = tl.reshape(mps, (2,2,2))
+    mps = matrix_product_state(mps, rank=[1,2,2,1])
+    tl_ee = tt_mps_entanglement_entropy(mps, 1)
+    tl.testing.assert_array_almost_equal(tl_ee, 0, decimal=3)
+
+def test_tt_mps_entanglement_entropy_entangled_state():
+    mps = tl.tensor([1,0,0,0,0,0,0,1])
+    mps = mps/tl.norm(mps)
+    mps = tl.reshape(mps, (2,2,2))
+    mps = matrix_product_state(mps, rank=[1,2,2,1])
+    tl_ee = tt_mps_entanglement_entropy(mps, 1)
+    tl.testing.assert_array_almost_equal(tl_ee, 1, decimal=3)
