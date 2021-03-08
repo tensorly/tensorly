e246fef017bfc390719d4c1bae6379289cc6ef7e
taylorpatti <taylorpatti@g.harvard.edu>
2021-03-08 09:44:23 -0500

    Updated naming conventions on testing suite

--- tensorly/metrics/tests/test_entropy.py
+++ tensorly/metrics/tests/test_entropy.py
@@ -3,21 +3,21 @@ from ..entropy import vonneumann_entropy, tt_mps_entanglement_entropy
 from ..entropy import tt_vonneumann_entropy, cp_vonneumann_entropy
 from ...decomposition import parafac, matrix_product_state
 
-state1 = tl.tensor([[0.03004805, 0.42426117, 0.5483771 , 0.4784077 , 0.25792725, 0.34388784, 0.99927586, 0.96605812]])
-state1 = state1/tl.norm(state1)
-state2 = tl.tensor([[0.84250089, 0.43429687, 0.26551928, 0.18262211, 0.55584835, 0.2565509 , 0.33197401, 0.97741178]])
-state2 = state2/tl.norm(state2)
-mat_mixed = tl.tensor((tl.dot(tl.transpose(state1), state1) + tl.dot(tl.transpose(state2), state2))/2.)
-actual_vne = 0.5546
 
-def test_vonNeumann_entropy_pure_state():
+def test_vonneumann_entropy_pure_state():
+    """Test for vonneumann_entropy on 2-dimensional tensors.
+    This test checks that pure states have a VNE of zero.
+    """
     state = tl.randn((8, 1))
     state = state/tl.norm(state)
     mat_pure = tl.dot(state, tl.transpose(state))
     tl_vne = vonneumann_entropy(mat_pure)
     tl.testing.assert_array_almost_equal(tl_vne, 0, decimal=3)
 
-def test_tt_vonNeumann_entropy_pure_state():
+def test_tt_vonneumann_entropy_pure_state():
+    """Test for tt_vonneumann_entropy TT tensors.
+    This test checks that pure states have a VNE of zero.
+    """
     state = tl.randn((8, 1))
     state = state/tl.norm(state)
     mat_pure = tl.reshape(tl.dot(state, tl.transpose(state)), (2, 2, 2, 2, 2, 2))
@@ -25,7 +25,10 @@ def test_tt_vonNeumann_entropy_pure_state():
     tl_vne = tt_vonneumann_entropy(mat_pure)
     tl.testing.assert_array_almost_equal(tl_vne, 0, decimal=3)
 
-def test_vonNeumann_entropy_pure_state_CP():
+def test_cp_vonneumann_entropy_pure_state():
+    """Test for cp_vonneumann_entropy on 2-dimensional CP tensors.
+    This test checks that pure states have a VNE of zero.
+    """
     state = tl.randn((8, 1))
     state = state/tl.norm(state)
     mat_pure = tl.dot(state, tl.transpose(state))
@@ -33,34 +36,63 @@ def test_vonNeumann_entropy_pure_state_CP():
     tl_vne = cp_vonneumann_entropy(mat)
     tl.testing.assert_array_almost_equal(tl_vne, 0, decimal=3)
 
-def test_vonNeumann_entropy_mixed_state():
+def test_vonneumann_entropy_mixed_state():
+    """Test for vonneumann_entropy on 2-dimensional tensors.
+    This test checks that the VNE of mixed states is calculated correctly.
+    """
+    state1 = tl.tensor([[0.03004805, 0.42426117, 0.5483771 , 0.4784077 , 0.25792725, 0.34388784, 0.99927586, 0.96605812]])
+    state1 = state1/tl.norm(state1)
+    state2 = tl.tensor([[0.84250089, 0.43429687, 0.26551928, 0.18262211, 0.55584835, 0.2565509 , 0.33197401, 0.97741178]])
+    state2 = state2/tl.norm(state2)
+    mat_mixed = tl.tensor((tl.dot(tl.transpose(state1), state1) + tl.dot(tl.transpose(state2), state2))/2.)
+    actual_vne = 0.5546
     tl_vne = vonneumann_entropy(mat_mixed)
     tl.testing.assert_array_almost_equal(tl_vne, actual_vne, decimal=3)
 
-def test_tt_vonNeumann_entropy_mixed_state():
+def test_tt_vonneumann_entropy_mixed_state():
+    """Test for tt_vonneumann_entropy on TT tensors.
+    This test checks that the VNE of mixed states is calculated correctly.
+    """
+    state1 = tl.tensor([[0.03004805, 0.42426117, 0.5483771 , 0.4784077 , 0.25792725, 0.34388784, 0.99927586, 0.96605812]])
+    state1 = state1/tl.norm(state1)
+    state2 = tl.tensor([[0.84250089, 0.43429687, 0.26551928, 0.18262211, 0.55584835, 0.2565509 , 0.33197401, 0.97741178]])
+    state2 = state2/tl.norm(state2)
+    mat_mixed = tl.tensor((tl.dot(tl.transpose(state1), state1) + tl.dot(tl.transpose(state2), state2))/2.)
+    actual_vne = 0.5546
     tt_mixed = tl.reshape(mat_mixed, (2, 2, 2, 2, 2, 2))
     tt_mixed = matrix_product_state(tt_mixed, rank=[1, 2, 4, 8, 4, 2, 1])
     tl_vne = tt_vonneumann_entropy(tt_mixed)
     tl.testing.assert_array_almost_equal(tl_vne, actual_vne, decimal=3)
 
-def test_vonNeumann_entropy_mixed_state_CP():
+def test_cp_vonneumann_entropy_mixed_state():
+    """Test for cp_vonneumann_entropy on CP tensors.
+    This test checks that the VNE of mixed states is calculated correctly.
+    """
+    state1 = tl.tensor([[0.03004805, 0.42426117, 0.5483771 , 0.4784077 , 0.25792725, 0.34388784, 0.99927586, 0.96605812]])
+    state1 = state1/tl.norm(state1)
+    state2 = tl.tensor([[0.84250089, 0.43429687, 0.26551928, 0.18262211, 0.55584835, 0.2565509 , 0.33197401, 0.97741178]])
+    state2 = state2/tl.norm(state2)
+    mat_mixed = tl.tensor((tl.dot(tl.transpose(state1), state1) + tl.dot(tl.transpose(state2), state2))/2.)
+    actual_vne = 0.5546
     mat = parafac(tl.tensor(mat_mixed), rank=2, normalize_factors=True)
+    mat_unnorm = parafac(tl.tensor(mat_mixed), rank=2, normalize_factors=False)
     tl_vne = cp_vonneumann_entropy(mat)
+    tl_vne_unnorm = cp_vonneumann_entropy(mat_unnorm)
     tl.testing.assert_array_almost_equal(tl_vne, actual_vne, decimal=3)
+    tl.testing.assert_array_almost_equal(tl_vne_unnorm, actual_vne, decimal=3)
 
-def test_vonNeumann_entropy_mixed_state_CP_unnormalized_factors():
-    mat = parafac(tl.tensor(mat_mixed), rank=2, normalize_factors=False)
-    tl_vne = cp_vonneumann_entropy(mat)
-    tl.testing.assert_array_almost_equal(tl_vne, actual_vne, decimal=3)
-
-def test_tt_mps_entanglement_entropy_product_state():
+def test_tt_mps_entanglement_entropy():
+    """Test for the tt_mps_entanglement_entropy on TT tensors.
+    This test checks that the EE of both product and entangled states is calculated correctly.
+    """
+    #Product state
     mps = tl.tensor([1,0,0,0,0,0,0,0])
     mps = tl.reshape(mps, (2,2,2))
     mps = matrix_product_state(mps, rank=[1,2,2,1])
     tl_ee = tt_mps_entanglement_entropy(mps, 1)
     tl.testing.assert_array_almost_equal(tl_ee, 0, decimal=3)
 
-def test_tt_mps_entanglement_entropy_entangled_state():
+    #Entangled state
     mps = tl.tensor([1,0,0,0,0,0,0,1])
     mps = mps/tl.norm(mps)
     mps = tl.reshape(mps, (2,2,2))
