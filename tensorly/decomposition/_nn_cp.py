fix normalization

tensorly/decomposition/_nn_cp.py
@@ -189,6 +189,8 @@ def non_negative_parafac(tensor, rank, n_iter_max=100, init='svd', svd='numpy_sv
     random_state : {None, int, np.random.RandomState}
     verbose : int, optional
         level of verbosity
+    normalize_factors : if True, aggregate the weights of each factor in a 1D-tensor
+        of shape (rank, ), which will contain the norms of the factors
     fixed_modes : list, default is None
         A list of modes for which the initial value is not modified.
         The last mode cannot be fixed due to error computation.
@@ -243,11 +245,11 @@ def non_negative_parafac(tensor, rank, n_iter_max=100, init='svd', svd='numpy_sv
                     accum *= tl.dot(tl.transpose(factors[e]), factors[e])
                 else:
                     accum = tl.dot(tl.transpose(factors[e]), factors[e])
-
+            accum = tl.reshape(weights, (-1, 1)) * accum * tl.reshape(weights, (1, -1))
             if mask is not None:
-                tensor = tensor * mask + tl.cp_to_tensor((None, factors), mask=1 - mask)
+                tensor = tensor * mask + tl.cp_to_tensor((weights, factors), mask=1 - mask)
 
-            mttkrp = unfolding_dot_khatri_rao(tensor, (None, factors), mode)
+            mttkrp = unfolding_dot_khatri_rao(tensor, (weights, factors), mode)
 
             numerator = tl.clip(mttkrp, a_min=epsilon, a_max=None)
             denominator = tl.dot(factors[mode], accum)
@@ -256,16 +258,13 @@ def non_negative_parafac(tensor, rank, n_iter_max=100, init='svd', svd='numpy_sv
 
             factors[mode] = factor
 
-        if normalize_factors:
-            weights, factors = cp_normalize((weights, factors))
-
         if tol:
             # ||tensor - rec||^2 = ||tensor||^2 + ||rec||^2 - 2*<tensor, rec>
             factors_norm = cp_norm((weights, factors))
 
             # mttkrp and factor for the last mode. This is equivalent to the
             # inner product <tensor, factorization>
-            iprod = tl.sum(tl.sum(mttkrp * factor, axis=0) * weights)
+            iprod = tl.sum(tl.sum(mttkrp * factor, axis=0))
             rec_error = tl.sqrt(tl.abs(norm_tensor**2 + factors_norm**2 - 2 * iprod)) / norm_tensor
             rec_errors.append(rec_error)
             if iteration >= 1:
@@ -288,7 +287,8 @@ def non_negative_parafac(tensor, rank, n_iter_max=100, init='svd', svd='numpy_sv
             else:
                 if verbose:
                     print('reconstruction error={}'.format(rec_errors[-1]))
-
+        if normalize_factors:
+            weights, factors = cp_normalize((weights, factors))
     cp_tensor = CPTensor((weights, factors))
 
     if return_errors:
@@ -299,7 +299,7 @@ def non_negative_parafac(tensor, rank, n_iter_max=100, init='svd', svd='numpy_sv
 
 def non_negative_parafac_hals(tensor, rank, n_iter_max=100, init="svd", svd='numpy_svd', tol=10e-8,
                               sparsity_coefficients=None, fixed_modes=None, nn_modes='all', exact=False,
-                              verbose=False, return_errors=False, cvg_criterion='abs_rec_error'):
+                              normalize_factors=False, verbose=False, return_errors=False, cvg_criterion='abs_rec_error'):
     """
     Non-negative CP decomposition via HALS
 
@@ -333,6 +333,8 @@ def non_negative_parafac_hals(tensor, rank, n_iter_max=100, init="svd", svd='num
     exact: If it is True, the algorithm gives a results with high precision but it needs high computational cost.
         If it is False, the algorithm gives an approximate solution
         Default: False
+    normalize_factors : if True, aggregate the weights of each factor in a 1D-tensor
+        of shape (rank, ), which will contain the norms of the factors
     verbose: boolean
         Indicates whether the algorithm prints the successive
         reconstruction errors or not
@@ -364,7 +366,7 @@ def non_negative_parafac_hals(tensor, rank, n_iter_max=100, init="svd", svd='num
 
     weights, factors = initialize_nn_cp(tensor, rank, init=init, svd=svd,
                                         random_state=None,
-                                        normalize_factors=False)
+                                        normalize_factors=normalize_factors)
 
     norm_tensor = tl.norm(tensor, 2)
 
@@ -404,17 +406,14 @@ def non_negative_parafac_hals(tensor, rank, n_iter_max=100, init="svd", svd='num
                 if i != mode:
                     pseudo_inverse = pseudo_inverse * tl.dot(tl.transpose(factor), factor)
 
-            if not iteration and weights is not None:
-                # Take into account init weights
-                mttkrp = unfolding_dot_khatri_rao(tensor, (weights, factors), mode)
-            else:
-                mttkrp = unfolding_dot_khatri_rao(tensor, (None, factors), mode)
+            pseudo_inverse = tl.reshape(weights, (-1, 1)) * pseudo_inverse * tl.reshape(weights, (1, -1))
+            mttkrp = unfolding_dot_khatri_rao(tensor, (weights, factors), mode)
 
             if mode in nn_modes:
                 # Call the hals resolution with nnls, optimizing the current mode
                 nn_factor, _, _, _ = hals_nnls(tl.transpose(mttkrp), pseudo_inverse, tl.transpose(factors[mode]),
-                                            n_iter_max=100, sparsity_coefficient=sparsity_coefficients[mode],
-                                            exact=exact)
+                                               n_iter_max=100, sparsity_coefficient=sparsity_coefficients[mode],
+                                               exact=exact)
                 factors[mode] = tl.transpose(nn_factor)
             else:
                 factor = tl.solve(tl.transpose(pseudo_inverse), tl.transpose(mttkrp))
@@ -444,7 +443,8 @@ def non_negative_parafac_hals(tensor, rank, n_iter_max=100, init="svd", svd='num
             else:
                 if verbose:
                     print('reconstruction error={}'.format(rec_errors[-1]))
-
+        if normalize_factors:
+            weights, factors = cp_normalize((weights, factors))
     cp_tensor = CPTensor((weights, factors))
     if return_errors:
         return cp_tensor, rec_errors