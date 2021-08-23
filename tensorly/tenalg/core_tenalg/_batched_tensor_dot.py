44fb25d6183e23e9bc353b067ff3e8646322733d
Jean Kossaifi <jean.kossaifi@gmail.com>
2021-08-23 20:23:59 +0100

    Typo

--- tensorly/tenalg/core_tenalg/_batched_tensor_dot.py
+++ tensorly/tenalg/core_tenalg/_batched_tensor_dot.py
@@ -42,13 +42,13 @@ def batched_tensor_dot(tensor1, tensor2, modes, batched_modes=()):
             free_counter += 1
 
     # We will reorganize tensor1 to (batch_modes, new_modes1, contraction_modes)
-    new_modes1 = [i for i in range(tensor1.ndim) if i not in batch_modes1+modes1]
+    new_modes1 = [i for i in range(tl.ndim(tensor1)) if i not in batch_modes1+modes1]
     new_shape1 = [tl.shape(tensor1)[i] for i in new_modes1]
     tensor1 = tl.transpose(tensor1, batch_modes1 + new_modes1 + modes1)
     tensor1 = tl.reshape(tensor1, (*batch_shape, -1, contraction_dim))
     
     # Tensor2 will be (batch_modes, contraction_modes, new_modes2)
-    new_modes2 = [i for i in range(tensor2.ndim) if i not in batch_modes2+modes2]
+    new_modes2 = [i for i in range(tl.ndim(tensor2)) if i not in batch_modes2+modes2]
     new_shape2 = [tl.shape(tensor2)[i] for i in new_modes2]
     tensor2 = tl.transpose(tensor2, batch_modes2+modes2+new_modes2)
     tensor2 = tl.reshape(tensor2, (*batch_shape, contraction_dim, -1))
