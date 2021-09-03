8cfb840a0503a397c4f4b0699d3d7b8e6b587f94
Aaron Meyer <ameyer@ucla.edu>
2021-09-03 09:49:01 -0700

    Slightly tighter testing

--- tensorly/decomposition/tests/test_cmtf_als.py
+++ tensorly/decomposition/tests/test_cmtf_als.py
@@ -24,17 +24,18 @@ def test_coupled_matrix_tensor_3d_factorization():
     X_pred, Y_pred, errors = coupled_matrix_tensor_3d_factorization(tensor_true, matrix_true, R)
 
     # Check that the error monotonically decreases
-    assert_(np.all(np.diff(errors) <= 1e-3))
+    assert_(np.all(np.diff(errors) <= 0.0))
 
     # # Check reconstruction of noisy tensor
-    # tol_norm_2 = 10e-2
-    # tol_max_abs = 10e-2
-    # tensor_pred = cp_to_tensor(X_pred)
-    # matrix_pred = cp_to_tensor(Y_pred)
-    # error = tl.norm(tensor_true - tensor_pred) ** 2 + tl.norm(matrix_true - matrix_pred) ** 2
+    tol_norm_2 = 10e-2
+    tol_max_abs = 10e-2
+    tensor_pred = cp_to_tensor(X_pred)
+    matrix_pred = cp_to_tensor(Y_pred)
+    error = tl.norm(tensor_true - tensor_pred) ** 2 + tl.norm(matrix_true - matrix_pred) ** 2
+    # TODO: These error checks do not always pass, possibly due to poor SVD initialization.
     # assert_(error < tol_norm_2,
     #         'norm 2 of reconstruction higher than tol')
-    # # Test the max abs difference between the reconstruction and the tensor
+    # Test the max abs difference between the reconstruction and the tensor
     # assert_(
     #     tl.max(tl.abs(tensor_true - tensor_pred)) + tl.max(
     #         tl.abs(matrix_true - matrix_pred)) < tol_max_abs,
