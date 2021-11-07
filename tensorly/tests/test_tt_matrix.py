8b446c75f0721740d9d44f4913cee043432e5322
Jean Kossaifi <jean.kossaifi@gmail.com>
2021-11-07 17:37:42 +0000

    FIX MXNet (no support transpose >6th order tensors)

--- tensorly/tests/test_tt_matrix.py
+++ tensorly/tests/test_tt_matrix.py
@@ -4,13 +4,13 @@ from ..tt_matrix import tt_matrix_to_matrix, tt_matrix_to_tensor, tt_matrix_to_v
 
 def test_tt_matrix_manipulation():
     """Test for tt_matrix manipulation"""
-    shape = (2, 2, 2, 3, 3, 3)
+    shape = (2, 2, 3, 3) # Revert to (2, 2, 2, 3, 3, 3) once MXNet supports transpose for > 6th order tensors
     tt_matrix = random.random_tt_matrix(shape, rank=2, full=False)
     rec = tt_matrix_to_tensor(tt_matrix)
     assert(tl.shape(rec) == shape)
 
     mat = tt_matrix_to_matrix(tt_matrix)
-    assert(tl.shape(mat) == (8, 27))
+    assert(tl.shape(mat) == (4, 9))
 
     vec = tt_matrix_to_vec(tt_matrix)
-    assert(tl.shape(vec) == (8*27,))
+    assert(tl.shape(vec) == (4*9,))
