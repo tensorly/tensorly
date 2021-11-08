9aaaac118717df02bb534fd10991a6b8e32527c0
Jean Kossaifi <jean.kossaifi@gmail.com>
2021-11-08 11:17:43 +0000

    FIX test_tt_matrix test

--- tensorly/tests/test_tt_matrix.py
+++ tensorly/tests/test_tt_matrix.py
@@ -11,12 +11,13 @@ skip_mxnet= pytest.mark.skipif(tl.get_backend() == "mxnet",
 def test_tt_matrix_manipulation():
     """Test for tt_matrix manipulation"""
     shape = (2, 2, 2, 3, 3, 3)
+    n_rows, n_cols = 8, 27
     tt_matrix = random.random_tt_matrix(shape, rank=2, full=False)
     rec = tt_matrix_to_tensor(tt_matrix)
     assert(tl.shape(rec) == shape)
 
     mat = tt_matrix_to_matrix(tt_matrix)
-    assert(tl.shape(mat) == (4, 9))
+    assert(tl.shape(mat) == (n_rows, n_cols))
 
     vec = tt_matrix_to_vec(tt_matrix)
-    assert(tl.shape(vec) == (4*9,))
+    assert(tl.shape(vec) == (n_rows*n_cols,))
