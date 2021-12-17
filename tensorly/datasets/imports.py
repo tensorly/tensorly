8ecb1e4cd5996549f6c26c86968a47c2f2c2f6be
Aaron Meyer <ameyer@ucla.edu>
2021-12-17 07:25:00 -0800

    Added a test

--- tensorly/datasets/imports.py
+++ tensorly/datasets/imports.py
@@ -2,10 +2,12 @@
 Load example datasets.
 """
 
+from os.path import dirname
 import numpy as np
 
 class Bunch(dict):
-    "Definition from scikit-learn"
+    """ A Bunch, exposing dict keys as a keys() meathod.
+    Definition from scikit-learn. """
     def __init__(self, **kwargs):
         super().__init__(kwargs)
 
@@ -26,21 +28,20 @@ class Bunch(dict):
 
 
 def IL2data():
-    """Loads tensor of cell-type specific responses to IL-2 mutants"""
-    tensor = np.load("data/IL2_Response_Tensor.npy")
+    """ Loads tensor of IL-2 mutein treatment responses. """
+    path_here = dirname(__file__)
+    tensor = np.load(path_here + "/data/IL2_Response_Tensor.npy")
     ligands = ['IL2 (Mono)', 'IL15 (Mono)', 'WT N-term (Biv)', 'H16N N-term (Biv)',
                   'R38Q N-term (Biv)', 'R38Q/H16N (Biv)', 'WT N-term (Mono)',
                   'H16N N-term (Mono)', 'WT C-term (Mono)', 'V91K C-term (Mono)',
                   'R38Q N-term (Mono)', 'F42Q N-Term (Mono)', 'N88D C-term (Mono)']
     cells = ['Treg IL2Ra_hi', 'Treg', 'Treg IL2Ra_lo', 'Thelper IL2Ra_hi', 'Thelper', 'Thelper IL2Ra_lo', 'CD8+', 'NK']
     times = [4, 2, 1, 0.5]
-    doses = [84, 28, 9.333333, 3.111, 1.037037, 0.345679, 0.115226, 0.038409, 0.012803, 0.004268, 0.001423, 0.000474]
-    dims = ["mutant", "time", "dose", "cells"]
+    doses = np.geomspace(84.0, 0.000474, 12)
+    dims = ["ligands", "times", "doses", "cells"]
     reference = ["Orcutt-Jahns, B., Emmel, P. C., Snyder, E. M., Posner, C., \
                 Carlson, S. M., & Meyer, A. S. (2021). Multivalency enhances the \
                 specificity of Fc-cytokine fusions. BioRxiv."]
-    task = ["Non-Negative PARAFAC Tensor Decomposition"]
+    task = ["Non-Negative CP"]
 
-    metadata = Bunch(tensor=tensor, ligands=ligands, cells=cells, times=times, doses=doses, dims=dims, reference=reference, task=task)
-
-    return metadata
+    return Bunch(tensor=tensor, ligands=ligands, cells=cells, times=times, doses=doses, dims=dims, reference=reference, task=task)
