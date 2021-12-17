2c93690696b268f45b4526b1d0a080d6cf233edc
Brian Orcutt-Jahns <orcuttjahnsbrian@gmail.com>
2021-12-17 15:45:20 -0800

    Add docstring, and .DESC to data

--- tensorly/datasets/imports.py
+++ tensorly/datasets/imports.py
@@ -6,7 +6,7 @@ from os.path import dirname
 import numpy as np
 
 class Bunch(dict):
-    """ A Bunch, exposing dict keys as a keys() meathod.
+    """ A Bunch, exposing dict keys as a keys() method.
     Definition from scikit-learn. """
     def __init__(self, **kwargs):
         super().__init__(kwargs)
@@ -28,7 +28,13 @@ class Bunch(dict):
 
 
 def IL2data():
-    """ Loads tensor of IL-2 mutein treatment responses. """
+    """ 
+    Loads tensor of IL-2 mutein treatment responses.
+    Tensor contains the signaling responses of eight different cell types to 13 IL-2 mutants.
+    Dose-responses to 12 concentrations of muteins were collected at four time points for each mutant and cell type.
+    Ligands include IL-2 mutants with modified receptor affinities, and in both mono- and bivalent formats.
+    Useful for demonstrating execution and interpretation of non-negative CP decomposition.
+    """
     path_here = dirname(__file__)
     tensor = np.load(path_here + "/data/IL2_Response_Tensor.npy")
     ligands = ['IL2 (Mono)', 'IL15 (Mono)', 'WT N-term (Biv)', 'H16N N-term (Biv)',
@@ -43,5 +49,6 @@ def IL2data():
                 Carlson, S. M., & Meyer, A. S. (2021). Multivalency enhances the \
                 specificity of Fc-cytokine fusions. BioRxiv."]
     task = ["Non-Negative CP"]
+    DESC = "This dataset includes a tensor of eight cell types' responses to 13 IL-2 muteins, at 4 different time points, and at 12 different IL-2 concentrations."
 
-    return Bunch(tensor=tensor, ligands=ligands, cells=cells, times=times, doses=doses, dims=dims, reference=reference, task=task)
+    return Bunch(tensor=tensor, ligands=ligands, cells=cells, times=times, doses=doses, dims=dims, reference=reference, task=task, DESC=DESC)
