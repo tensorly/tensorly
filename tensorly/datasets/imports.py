4ed787748df1bf786a885eb70551d9dcad273bd8
Aaron Meyer <ameyer@ucla.edu>
2021-12-20 10:59:05 -0800

    Added license to Bunch

--- tensorly/datasets/imports.py
+++ tensorly/datasets/imports.py
@@ -5,9 +5,11 @@ Load example datasets.
 from os.path import dirname
 import numpy as np
 
+
 class Bunch(dict):
     """ A Bunch, exposing dict keys as a keys() method.
     Definition from scikit-learn. """
+
     def __init__(self, **kwargs):
         super().__init__(kwargs)
 
@@ -28,7 +30,7 @@ class Bunch(dict):
 
 
 def IL2data():
-    """ 
+    """
     Loads tensor of IL-2 mutein treatment responses.
     Tensor contains the signaling responses of eight different cell types to 13 IL-2 mutants.
     Dose-responses to 12 concentrations of muteins were collected at four time points for each mutant and cell type.
@@ -38,9 +40,9 @@ def IL2data():
     path_here = dirname(__file__)
     tensor = np.load(path_here + "/data/IL2_Response_Tensor.npy")
     ligands = ['IL2 (Mono)', 'IL15 (Mono)', 'WT N-term (Biv)', 'H16N N-term (Biv)',
-                  'R38Q N-term (Biv)', 'R38Q/H16N (Biv)', 'WT N-term (Mono)',
-                  'H16N N-term (Mono)', 'WT C-term (Mono)', 'V91K C-term (Mono)',
-                  'R38Q N-term (Mono)', 'F42Q N-Term (Mono)', 'N88D C-term (Mono)']
+               'R38Q N-term (Biv)', 'R38Q/H16N (Biv)', 'WT N-term (Mono)',
+               'H16N N-term (Mono)', 'WT C-term (Mono)', 'V91K C-term (Mono)',
+               'R38Q N-term (Mono)', 'F42Q N-Term (Mono)', 'N88D C-term (Mono)']
     cells = ['Treg IL2Ra_hi', 'Treg', 'Treg IL2Ra_lo', 'Thelper IL2Ra_hi', 'Thelper', 'Thelper IL2Ra_lo', 'CD8+', 'NK']
     times = [4, 2, 1, 0.5]
     doses = np.geomspace(84.0, 0.000474, 12)
@@ -50,5 +52,34 @@ def IL2data():
                 specificity of Fc-cytokine fusions. BioRxiv."]
     task = ["Non-Negative CP"]
     DESC = "This dataset includes a tensor of eight cell types' responses to 13 IL-2 muteins, at 4 different time points, and at 12 different IL-2 concentrations."
+    LICENSE = """Copyright 2021 The Regents of the University of California
 
-    return Bunch(tensor=tensor, ligands=ligands, cells=cells, times=times, doses=doses, dims=dims, reference=reference, task=task, DESC=DESC)
+        Permission is hereby granted, free of charge, to any person obtaining a copy
+        of this software and associated documentation files (the "Software"), to deal
+        in the Software without restriction, including without limitation the rights
+        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        copies of the Software, and to permit persons to whom the Software is
+        furnished to do so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be included in all
+        copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        SOFTWARE."""
+
+    return Bunch(
+        tensor=tensor,
+        ligands=ligands,
+        cells=cells,
+        times=times,
+        doses=doses,
+        dims=dims,
+        reference=reference,
+        task=task,
+        DESC=DESC,
+        LICENSE=LICENSE)
