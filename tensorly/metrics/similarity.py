26517f138ec806673456bd55c798c238acb3fac6
hmbaghdassarian <hmbaghdassarian@eng.ucsd.edu>
2022-01-19 11:01:52 -0800

    use tensorly operations instead of numpy

--- tensorly/metrics/similarity.py
+++ tensorly/metrics/similarity.py
@@ -1,7 +1,8 @@
 from typing import List
 
 import numpy as np
-from sklearn.preprocessing import normalize
+
+import tensorly as tl
 
 # Authors: Hratch Baghdassarian <hmbaghdassarian@gmail.com>, Erick Armingol <earmingol14@gmail.com>
 # similarity metrics for tensor decompsoitions
@@ -19,10 +20,10 @@ def stack_loading_matrices(factors: List[np.ndarray]) -> np.ndarray:
     np.ndarray
         A vertically stacked matrix of shape [sum(tensor.shape)xR)] 
     """
-    
-    if not np.all([A.shape[1] for A in factors]):
+
+    if len({A.shape[1] for A in factors}) != 1:
         raise ValueError('factors should be a list of loading matrices')
-    return np.vstack(factors)
+    return tl.concatenate(factors)
 
 def calc_corrindex(C: np.ndarray) -> float:
     """Calculate the CorrIndex.
@@ -38,14 +39,16 @@ def calc_corrindex(C: np.ndarray) -> float:
     float
         CorrIndex metric [0,1]; lower score means higher between matrices
     """
-    
-    MN = C.shape[1]*C.shape[0]
-    return (1/(MN))*(np.sum(np.abs(np.amax(C, 1) - 1)) + np.sum(np.abs(np.amax(C, 0) - 1)))
+    if C.shape[1] != C.shape[0]:
+        raise ValueError('C must be a square matrix')
+
+    MN = C.shape[1]*C.shape[0] #TODO: N notation in equation 8 unclear, make sure this is correct
+    return (1/(MN))*(tl.sum(tl.abs(tl.max(C,1) - 1)) + tl.sum(tl.abs(tl.max(C, 0) - 1)))
 
 def corrindex(factors_1: List[np.ndarray], factors_2: List[np.ndarray], tol: float = 5e-16) -> float:
     """CorrIndex implementation to assess tensor decomposition outputs. 
     From Sobhani et al 2022 (https://doi.org/10.1016/j.sigpro.2022.108457). 
-    Metric is scaling and column-permutation invariant.  
+    Metric is scaling and column-permutation invariant. 
 
     Parameters
     ----------
@@ -55,7 +58,7 @@ def corrindex(factors_1: List[np.ndarray], factors_2: List[np.ndarray], tol: flo
         The loading/factor matrices [A^1 ... A^R] for a low-rank tensor from its factors, output from second decomposition
     tol : float, optional
         precision threshold below to call the CorrIndex score 0, by default 1e-16
-        
+
     Returns
     -------
     score : float
@@ -64,17 +67,16 @@ def corrindex(factors_1: List[np.ndarray], factors_2: List[np.ndarray], tol: flo
     # get stacked loading matrices
     X_1 = stack_loading_matrices(factors_1)
     X_2 = stack_loading_matrices(factors_2)
-    
-    if X_1.shape != X_2.shape: 
+
+    if X_1.shape != X_2.shape:
         raise ValueError('Factor matrices should be of the same shapes')
-    
+
     # normalize columns to L2 norm - even if ran decomposition with normalize_factors = True  
-#   X/np.apply_along_axis(np.linalg.norm, 0, X) 
-    X_1 = normalize(X_1, norm = 'l2', axis = 0)
-    X_2 = normalize(X_2, norm = 'l2', axis = 0)
-    
-    C = np.abs(np.matmul(np.matrix(X_1).H, X_2))
-    
+    X_1 = X_1/tl.norm(X_1, axis = 0)
+    X_2 = X_2/tl.norm(X_2, axis = 0)
+
+    C = tl.abs(tl.matmul(tl.conj(X_1.T), X_2))
+
     score = calc_corrindex(C)
     if score < tol:
         score = 0
