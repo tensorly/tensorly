c55cc62bc6081221eae9306fb6f2dcaa4d991f34
Erick Armingol <earmingol14@gmail.com>
2022-02-08 16:59:27 -0800

    Implemented multiple methods to compute the correlation index

--- tensorly/metrics/__init__.py
+++ tensorly/metrics/__init__.py
@@ -6,3 +6,4 @@ The :mod:`tensorly.metrics` module includes utilities to measure performance
 from .regression import RMSE, MSE
 from .entropy import vonneumann_entropy, tt_vonneumann_entropy, cp_vonneumann_entropy
 from .factors import congruence_coefficient
+from .similarity import correlation_index
