c53aaa5a3574b695d1570ca37d2d7b522e33b0b1
Lili <lilizheng@stat.wisc.edu>
2022-06-15 13:11:00 -0400

    Modify test_tt_TTOI.py

--- tensorly/contrib/decomposition/tests/test_tt_TTOI.py
+++ tensorly/contrib/decomposition/tests/test_tt_TTOI.py
@@ -12,7 +12,6 @@ import tensorly as tl
 from tensorly import random
 from tensorly.testing import assert_, assert_class_wrapper_correctly_passes_arguments
 from tensorly.decomposition._tt import TensorTrain, tensor_train
-import tensorly.decomposition
 from tensorly.contrib.decomposition.tt_TTOI import tensor_train_OI, TensorTrain_OI
 
 
@@ -34,12 +33,12 @@ def test_TTOI(monkeypatch):
     factors_list, full_tensor_list, approx_errors = tensor_train_OI(data_tensor = data_tensor, rank = rank, n_iter = n_iter, trajectory = True, return_errors = True)
     
     # Check that the approximation error monotonically decreases
-    approx_errors /= tl.norm(data_tensor, 2)
+    approx_errors = tl.tensor(approx_errors) / tl.norm(data_tensor, 2)
     assert_(np.all(np.diff(approx_errors) <= 1e-3))
     
     # check that the estimation error of TTOI improves from initialization (TTSVD) 
     estimation_errors = [tl.norm(full_tensor_list[i]-true_tensor,2) for i in range(n_iter*2)]
-    estimation_errors /= tl.norm(true_tensor, 2)
+    estimation_errors = tl.tensor(estimation_errors) / tl.norm(true_tensor, 2)
     assert_(estimation_errors[0]-estimation_errors[2*n_iter-1] >= 1e-3)
 
     assert_class_wrapper_correctly_passes_arguments(monkeypatch, tensor_train_OI, TensorTrain_OI, ignore_args = {}, rank = rank)
