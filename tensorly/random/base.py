4ba6bb6acfd95d13a277f15b6fa61012a1ef0bcd
bakhtos <68953985+bakhtos@users.noreply.github.com>
2022-08-08 16:36:43 +0300

    fix: change to support Python 3.7
    Co-authored-by: Marie Roald <roald.marie@gmail.com>

--- tensorly/random/base.py
+++ tensorly/random/base.py
@@ -202,7 +202,7 @@ def random_tt(shape, rank, full=False, random_state=None, **context):
         message = f"Provided rank[0] = {rank[0]} but boundaring conditions dictatate rank[0] == rank[-1] == 1: setting rank[0] to 1."
         raise ValueError(message)
     if rank[-1] != 1:
-        message = f"Provided {rank[-1] = } but boundaring conditions dictatate rank[0] == rank[-1] == 1: setting rank[-1] to 1."
+        message = f"Provided rank[-1] = {rank[-1]} but boundaring conditions dictatate rank[0] == rank[-1] == 1: setting rank[-1] to 1."
         raise ValueError(message)
 
     rns = T.check_random_state(random_state)
