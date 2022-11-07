e248acc7306d169413201a08f25f8575018fe617
Lili <lilizheng@stat.wisc.edu>
2022-11-06 23:32:59 -0500

    Make stylistic changes suggested by @cohenjer

--- tensorly/contrib/decomposition/tt_TTOI.py
+++ tensorly/contrib/decomposition/tt_TTOI.py
@@ -48,7 +48,6 @@ def tensor_train_OI(
     data_tensor, rank, n_iter=1, trajectory=False, return_errors=True, **context
 ):
     """Perform tensor-train orthogonal iteration (TTOI) for tensor train decomposition
-    Reference paper: Zhou Y, Zhang AR, Zheng L, Wang Y. "Optimal high-order tensor svd via tensor-train orthogonal iteration."
 
     Parameters
     ----------
@@ -67,12 +66,17 @@ def tensor_train_OI(
         if True, the approximation/reconstruction error of each iteration of TTOI is returned: 2*n_iter outputs
     Returns
     -------
-    factors : list of n_iter tensors or one tensor
-    * n_iter tensors (if `trajectory` is True) : each list contains the output of each iteration, one full tensor and list of tensor factors
-    * one tensor (otherwise): output of the last iteration, one full tensor and list of tensor factors 
+    factors : list of n_iter lists of tensor-train factors or one list of tensor-train factors
+    * n_iter lists of factors (if `trajectory` is True) : each list contains the returned list of tensor-train factors from each iteration
+    * one list of factors (otherwise): returned list of tensor-train factors from the last iteration
     full_tensor : list of n_iter tensors or one tensor
-    * n_iter tensors (if `trajectory` is True) : each list contains the output of each iteration, one full tensor and list of tensor factoros
-    * one tensor (otherwise): output of the last iteration, one full tensor and list of tensor factors 
+    * n_iter tensors (if `trajectory` is True) : each list contains the returned full tensor from each iteration
+    * one tensor (otherwise): returned full tensor from the last iteration
+    
+    References
+    ----------
+    .. [1] Zhou, Y., Zhang, A. R., Zheng, L., & Wang, Y. (2022). Optimal high-order tensor svd via tensor-train orthogonal iteration. 
+    IEEE Transactions on Information Theory, 68(6), 3991-4019.
     """
     context = tl.context(data_tensor)
     shape = tl.shape(data_tensor)
@@ -87,19 +91,19 @@ def tensor_train_OI(
     data_tensor_extended = tl.reshape(data_tensor, (1,) + shape + (1,))
 
     if trajectory:
-        factors = list()
-        full_tensor = list()
+        factors = []
+        full_tensor = []
 
     if return_errors:
-        error_list = list()
+        error_list = []
 
     # perform TTOI for n_iter iterations
     for iteration in range(n_iter):
         # first perform forward update
         # left_singular_vectors will be a list including estimated left singular spaces at the current iteration
-        left_singular_vectors = list()
+        left_singular_vectors = []
         # initialize left_residuals (sequential unfolding of data_tensor multiplied by left_singular_vectors sequentially on the left, useful for backward update to obtain right_singular_vectors)
-        left_residuals = list()
+        left_residuals = []
 
         # estimate the first left singular spaces
         # Here, R_tmp is the first sequential unfolding compressed on the right by previous updated right_singular_vectors (if exists)
@@ -148,7 +152,7 @@ def tensor_train_OI(
             )
         )
         if trajectory or return_errors:
-            factors_list_tmp = list()
+            factors_list_tmp = []
             for mode in range(n_dim - 1):
                 factors_list_tmp.append(
                     tl.tensor(left_singular_vectors[mode], **context)
@@ -163,7 +167,7 @@ def tensor_train_OI(
 
         # perform backward update
         # initialize right_singular_vectors: right_singular_vectors will be a list of estimated right singular spaces at the current or previous iteration
-        right_singular_vectors = list()
+        right_singular_vectors = []
         V_tmp = tl.transpose(
             tl.tenalg.svd_interface(
                 matrix=tl.reshape(
@@ -211,7 +215,7 @@ def tensor_train_OI(
             data_tensor_extended, right_singular_vectors, left_to_right=False
         )
         if trajectory or return_errors or iteration == n_iter - 1:
-            factors_list_tmp = list()
+            factors_list_tmp = []
             factors_list_tmp.append(tl.tensor(Residual_right, **context))
             for mode in range(n_dim - 1):
                 factors_list_tmp.append(
