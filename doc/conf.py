7fc82e49f9ce723d2698bc101b015fd175f4d994
Jean Kossaifi <jean.kossaifi@gmail.com>
2022-12-31 19:34:33 +0100

    DOC: remove permalinks

--- doc/conf.py
+++ doc/conf.py
@@ -181,7 +181,7 @@ html_short_title = 'TensorLy'
 # of the sidebar.
 html_logo = '_static/logos/logo_tensorly.png'
 
-html_add_permalinks = ''
+html_permalinks = False
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
