7f005db57fb841205c0adc146f19c587a7f6a30a
COHEN Jeremy <jeremy.cohen@cnrs.fr>
2023-01-03 14:53:17 +0100

    adding indian pines locally and updating loader

--- tensorly/datasets/tests/test_imports.py
+++ tensorly/datasets/tests/test_imports.py
@@ -28,7 +28,7 @@ def test_COVID19_data():
     assert tensor.shape[2] == len(data["ticks"][2])
 
 
-@pytest.mark.skip(reason="currently failing, issue #465")
+#@pytest.mark.skip(reason="currently failing, issue #465")
 def test_indian_pines():
     """Test that data import dimensions match."""
     data = fetch_indian_pines()
