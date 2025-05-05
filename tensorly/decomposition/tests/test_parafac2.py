ba51dff541fd7bd1d8bc1cd0e1d9570ca85db11b
Christos Chatzis <cchatzis@outlook.com>
2025-05-05 18:50:48 +0200

    Update tensorly/decomposition/tests/test_parafac2.py
    Co-authored-by: Aaron Meyer <2065146+aarmey@users.noreply.github.com>

--- tensorly/decomposition/tests/test_parafac2.py
+++ tensorly/decomposition/tests/test_parafac2.py
@@ -548,7 +548,7 @@ def test_parafac2_em(linesearch):
 
     # assert FMS > 0.98
     A_sign = T.sign(random_parafac2_tensor.factors[0])
-    rec_A_sign = T.sign(rec.factors[0])
+    rec_A_sign = tl.sign(rec.factors[0])
     A_corr = congruence_coefficient(
         A_sign * random_parafac2_tensor.factors[0], rec_A_sign * rec.factors[0]
     )[0]
