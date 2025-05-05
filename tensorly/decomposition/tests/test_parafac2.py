79f82841f02940d2fcd50318314d58c3e78e7064
Christos Chatzis <cchatzis@outlook.com>
2025-05-05 18:50:38 +0200

    Update tensorly/decomposition/tests/test_parafac2.py
    Co-authored-by: Aaron Meyer <2065146+aarmey@users.noreply.github.com>

--- tensorly/decomposition/tests/test_parafac2.py
+++ tensorly/decomposition/tests/test_parafac2.py
@@ -562,7 +562,7 @@ def test_parafac2_em(linesearch):
     for i, (true_proj, rec_proj) in enumerate(
         zip(random_parafac2_tensor.projections, rec.projections)
     ):
-        true_Bi = T.dot(true_proj, random_parafac2_tensor.factors[1]) * A_sign[i]
+        true_Bi = tl.dot(true_proj, random_parafac2_tensor.factors[1]) * A_sign[i]
         rec_Bi = tl.dot(rec_proj, rec.factors[1]) * rec_A_sign[i]
         Bi_corr = congruence_coefficient(true_Bi, rec_Bi)[0]
         assert_(Bi_corr > 0.98)
